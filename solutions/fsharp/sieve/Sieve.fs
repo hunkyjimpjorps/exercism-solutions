0a28fc7a34da58aa099d992a53982db9737eea41
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:12 +0000

    [Sync Iteration] fsharp sieve

--- solutions/fsharp/sieve/Sieve.fs
+++ solutions/fsharp/sieve/Sieve.fs
@@ -3,20 +3,7 @@
 let primes limit =
     let rec nextSieve candidates acc =
         match candidates with
-        // The list will be empty if limit < 2
-        | [] -> acc
-        // We can stop iterating over the list once we reach a head value whose square
-        // is greater than the limit, since all its multiples will have been already
-        // sieved by earlier steps
-        // Since we pre-sieved out 2 when making the initial list, we add it back in now
-        | h :: t when pown h 2 > limit -> 2 :: (List.rev acc @ candidates)
-        // For the same reason, we don't have to check to filter out multiples lower than h * h
-        // since 2h, 3h, 4h... will have already been removed
-        // We also don't need to check even multiples
-        | h :: t -> nextSieve (List.except [ pown h 2 .. 2 * h .. limit ] t) (h :: acc)
+        | [] -> acc |> List.rev
+        | h :: t -> nextSieve (List.except [ 2 * h .. h .. limit ] t) (h :: acc)
 
-    match limit with
-    | n when n < 2 -> []
-    | 2 -> [ 2 ]
-    // Pre-filter all even numbers to eliminate the most intensive step
-    | _ -> nextSieve [ 3 .. 2 .. limit ] List.empty
+    nextSieve [ 2 .. limit ] List.empty
