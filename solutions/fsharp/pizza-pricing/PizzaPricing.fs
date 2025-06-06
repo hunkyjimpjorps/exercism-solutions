e92fb8b836418bf52811eea49ccc9fe0e8abbe48
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:37 +0000

    [Sync Iteration] fsharp pizza-pricing

---
+++ solutions/fsharp/pizza-pricing/PizzaPricing.fs
@@ -0,0 +1,25 @@
+module PizzaPricing
+
+type Pizza =
+    | Margherita
+    | Caprese
+    | Formaggio
+    | ExtraSauce of Pizza
+    | ExtraToppings of Pizza
+
+let rec pizzaPrice (pizza: Pizza) : int =
+    match pizza with
+    | Margherita -> 7
+    | Caprese -> 9
+    | Formaggio -> 10
+    | ExtraSauce p -> pizzaPrice p + 1
+    | ExtraToppings p -> pizzaPrice p + 2
+
+let orderPrice (pizzas: Pizza list) : int =
+    let surcharge =
+        match pizzas with
+        | [ _ ] -> 3
+        | [ _; _ ] -> 2
+        | _ -> 0
+
+    List.fold (fun acc p -> acc + pizzaPrice p) surcharge pizzas
