f5bc0e523d3de990621ee6ea74b4c74c36f82d37
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:56:59 +0000

    [Sync Iteration] elixir killer-sudoku-helper

--- solutions/elixir/killer-sudoku-helper/lib/killer_sudoku_helper.ex
+++ solutions/elixir/killer-sudoku-helper/lib/killer_sudoku_helper.ex
@@ -3,10 +3,11 @@ defmodule KillerSudokuHelper do
   Return the possible combinations of `size` distinct numbers from 1-9 excluding `exclude` that sum up to `sum`.
   """
   @spec combinations(cage :: %{exclude: [integer], size: integer, sum: integer}) :: [[integer]]
-  def combinations(%{exclude: exclude, size: size, sum: sum}),
-    do: (Enum.to_list(1..9) -- exclude) |> combine(size) |> Enum.filter(&(Enum.sum(&1) == sum))
-
-  defp combine(_, 0), do: [[]]
-  defp combine([], _), do: []
-  defp combine([h | t], n), do: for(rest <- combine(t, n - 1), do: [h | rest]) ++ combine(t, n)
+  def combinations(%{exclude: exclude, size: size, sum: sum}) do
+    Enum.to_list(1..9) -- exclude
+    |> Combination.combine(size)
+    |> Enum.map(&Enum.sort/1)
+    |> Enum.sort_by(&hd/1)
+    |> Enum.filter(&(Enum.sum(&1) == sum))
+  end
 end
