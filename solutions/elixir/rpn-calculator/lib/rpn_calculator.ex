5edfd661505db3b75c1040bb752d09207da3ac18
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:58 +0000

    [Sync Iteration] elixir rpn-calculator

---
+++ solutions/elixir/rpn-calculator/lib/rpn_calculator.ex
@@ -0,0 +1,21 @@
+defmodule RPNCalculator do
+  def calculate!(stack, operation) do
+    operation.(stack)
+  end
+
+  def calculate(stack, operation) do
+    try do
+      operation.(stack)
+    rescue
+      err in _ -> :error
+    end
+  end
+
+  def calculate_verbose(stack, operation) do
+    try do
+      operation.(stack)
+    rescue
+      err in _ -> {:error, err.message}
+    end
+  end
+end
