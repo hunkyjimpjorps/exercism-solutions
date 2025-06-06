00c0eba40a84fd5b4e79460870cc9b8da02eb372
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:29 +0000

    [Sync Iteration] fsharp difference-of-squares

---
+++ solutions/fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,14 @@
+﻿module DifferenceOfSquares
+
+let squareOfSum (number: int) : int =
+    seq { 1 .. number }
+    |> Seq.sum
+    |> (fun x -> pown x 2)
+
+let sumOfSquares (number: int) : int =
+    seq { 1 .. number }
+    |> Seq.map (fun x -> pown x 2)
+    |> Seq.sum
+
+let differenceOfSquares (number: int) : int =
+    squareOfSum number - sumOfSquares number
