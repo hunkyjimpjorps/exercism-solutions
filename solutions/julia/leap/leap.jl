47f2f84e71e8b7d2bf934218af6b33e94a6910b7
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:58:05 +0000

    [Sync Iteration] julia leap

--- solutions/julia/leap/leap.jl
+++ solutions/julia/leap/leap.jl
@@ -5,14 +5,14 @@ Return `true` if `year` is a leap year in the gregorian calendar.
 
 """
 function is_leap_year(year)
-    if year % 400 == 0
+    if year % 4 !== 0
+        false
+    elseif year % 400 == 0
         true
     elseif year % 100 == 0
         false
-    elseif year % 4 == 0
-        true
     else 
-        false
+        true
     end
 end
 
