21d9f4a6455f18ea62349c33220770fb356b8113
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:31 +0000

    [Sync Iteration] fsharp acronym

---
+++ solutions/fsharp/acronym/Acronym.fs
@@ -0,0 +1,13 @@
+﻿module Acronym
+
+open FsRegEx
+open FSharpx
+
+let abbreviate phrase =
+    phrase
+    |> replace "[.,_]" ""
+    |> replace "(\\s-\\s|-|\\s+)" " "
+    |> String.splitChar [| ' ' |]
+    |> Array.map (Seq.head >> string)
+    |> System.String.Concat
+    |> String.toUpper
