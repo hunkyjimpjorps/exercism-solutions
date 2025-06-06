53a6d59a756de3bcbc92223e203f2055367034fe
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:45 +0000

    [Sync Iteration] elixir prime-factors

--- solutions/elixir/prime-factors/lib/prime_factors.ex
+++ solutions/elixir/prime-factors/lib/prime_factors.ex
@@ -8,13 +8,23 @@ defmodule PrimeFactors do
   The prime factors of 'number' will be ordered lowest to highest.
   """
   @spec factors_for(pos_integer) :: [pos_integer]
-  def factors_for(1, _), do: []
+  def factors_for(number, divisor \\ 2, factors \\ [])
 
-  def factors_for(number, divisor \\ 2) do
+  def factors_for(1, _, factors), do: Enum.reverse(factors)
+
+  def factors_for(number, divisor, factors) do
     if rem(number, divisor) == 0 do
-      [divisor | factors_for(div(number, divisor), divisor)]
+      factors_for(
+        div(number, divisor),
+        divisor,
+        [divisor | factors]
+      )
     else
-      factors_for(number, divisor + 1)
+      factors_for(
+        number,
+        divisor + 1,
+        factors
+      )
     end
   end
 end
