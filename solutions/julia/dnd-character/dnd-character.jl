f2309ec544a7921221486114e1c4d3877b9d45b8
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:58 +0000

    [Sync Iteration] julia dnd-character

---
+++ solutions/julia/dnd-character/dnd-character.jl
@@ -0,0 +1,25 @@
+using Pipe
+
+function modifier(ability)
+    floor((ability - 10) / 2)
+end
+
+function ability()
+    @pipe rand(1:6, 4) |> sort |> _[2:4] |> sum
+end
+
+mutable struct DNDCharacter
+    strength
+    dexterity
+    constitution
+    intelligence
+    wisdom
+    charisma
+    hitpoints
+    function DNDCharacter()
+        con = ability()
+        new(ability(), ability(), con, ability(), ability(), ability(), 10 + modifier(con))
+    end
+end
+
+
