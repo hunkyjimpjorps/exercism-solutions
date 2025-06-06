53d89aa6854e98a367318c3211f258ce94b8b679
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:56 +0000

    [Sync Iteration] elixir high-score

--- solutions/elixir/high-score/lib/high_score.ex
+++ solutions/elixir/high-score/lib/high_score.ex
@@ -1,8 +1,8 @@
 defmodule HighScore do
-  def new(), do: %{}
+  @new_highscore_list %{}
+  def new(), do: @new_highscore_list
 
-  def add_player(scores, name, score), do: Map.put(scores, name, score)
-  def add_player(scores, name), do: Map.put(scores, name, 0)
+  def add_player(scores, name, score \\ 0), do: Map.put(scores, name, score)
 
   def remove_player(scores, name), do: Map.delete(scores, name)
 
