fb1c2198215f6853f450c91cdd6786cd8234efd5
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:35 +0000

    [Sync Iteration] fsharp darts

---
+++ solutions/fsharp/darts/Darts.fs
@@ -0,0 +1,10 @@
+﻿module Darts
+
+let (|RadiusGreaterThan|) r (x, y) : bool = sqrt (x ** 2.0 + y ** 2.0) > r
+
+let score (x: double) (y: double) : int =
+    match x, y with
+    | RadiusGreaterThan 10.0 true -> 0
+    | RadiusGreaterThan 5.0 true -> 1
+    | RadiusGreaterThan 1.0 true -> 5
+    | _ -> 10
