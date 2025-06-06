496f01b3c1f99b4249109efeefb3c38f4ff0f8c7
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:05 +0000

    [Sync Iteration] elixir resistor-color-trio

--- solutions/elixir/resistor-color-trio/lib/resistor_color_trio.ex
+++ solutions/elixir/resistor-color-trio/lib/resistor_color_trio.ex
@@ -16,25 +16,18 @@ defmodule ResistorColorTrio do
     white: 9
   }
 
-  @powers %{
-    0 => :ohms,
-    3 => :kiloohms,
-    6 => :megaohms,
-    9 => :gigaohms
-  }
-
-  @spec label(colors :: [atom]) :: {number, :ohms | :kiloohms | :megaohms | :gigaohms}
+  @spec label(colors :: [atom]) :: {number, :ohms | :kiloohms}
   def label(colors) do
     colors
     |> Enum.map(fn c -> @colors[c] end)
-    |> (fn [tens, ones, power | _] ->
-          (tens * 10 + ones) * Integer.pow(10, power)
-        end).()
+    |> compute()
     |> (fn
-          0 -> {0, :ohms}
-          r -> 
-            base = 3 * floor(:math.log10(r) / 3)
-            {r / (10 ** base), @powers[base]}
+          r when r > 1000 -> {r / 1000, :kiloohms}
+          r -> {r, :ohms}
         end).()
+  end
+
+  defp compute([tens, ones, power]) do
+    (tens * 10 + ones) * Integer.pow(10, power)
   end
 end
