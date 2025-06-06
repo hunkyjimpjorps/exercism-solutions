9bc25586726141d884d2f78606aef365977dc917
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:28 +0000

    [Sync Iteration] fsharp grade-school

---
+++ solutions/fsharp/grade-school/GradeSchool.fs
@@ -0,0 +1,24 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty : School = Map.empty
+
+let add (student: string) (grade: int) (school: School) : School =
+    match Map.containsKey grade school with
+    | true -> Map.add grade (student :: school.[grade]) school
+    | false -> Map.add grade [ student ] school
+
+let roster (school: School) : string list =
+    school
+    |> Map.toList
+    |> List.map (fun x -> snd x)
+    |> List.map (List.sort)
+    |> List.concat
+
+let grade (number: int) (school: School) : string list =
+    school
+    |> Map.containsKey number
+    |> function
+    | true -> school.[number] |> List.sort
+    | false -> []
