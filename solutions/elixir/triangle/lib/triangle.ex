5812242919f540a76d1e1daa89881e8cad16c7f5
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:41 +0000

    [Sync Iteration] elixir triangle

---
+++ solutions/elixir/triangle/lib/triangle.ex
@@ -0,0 +1,29 @@
+defmodule Triangle do
+  @type kind :: :equilateral | :isosceles | :scalene
+
+  defguardp invalid_inequality(a, b, c)
+            when a + b <= c or
+                   b + c <= a or
+                   a + c <= b
+
+  defguardp invalid_sides(a, b, c) when a <= 0 or b <= 0 or c <= 0
+
+  @doc """
+  Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
+  """
+  @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
+
+  def kind(a, b, c) when invalid_sides(a, b, c),
+    do: {:error, "all side lengths must be positive"}
+
+  def kind(a, b, c) when invalid_inequality(a, b, c),
+    do: {:error, "side lengths violate triangle inequality"}
+
+  def kind(a, b, c) do
+    case [a, b, c] do
+      [n, n, n] -> {:ok, :equilateral}
+      [m, n, p] when m == n or n == p or m == p -> {:ok, :isosceles}
+      _ -> {:ok, :scalene}
+    end
+  end
+end
