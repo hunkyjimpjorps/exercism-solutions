9cbf8c966325fc1719fcf7965d535a89a5b08ca8
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:52:06 +0000

    [Sync Iteration] r word-count

--- solutions/r/word-count/word-count.R
+++ solutions/r/word-count/word-count.R
@@ -1,22 +1,7 @@
 library(purrr)
 library(stringr)
-
-list_upsert <- function(lst, k, f, v0) {
-  if (is.null(lst[[k]])) {
-    lst[[k]] <- v0
-  } else {
-    lst[[k]] <- f(lst[[k]])
-  }
-  lst
-}
-
-add_1 <- function(n) {
-  n + 1
-}
-
-tally <- function(lst, word) {
-  list_upsert(lst, word, add_1, 1)
-}
+library(dplyr)
+library(tibble)
 
 word_count <- function(input) {
   input %>%
@@ -24,5 +9,9 @@ word_count <- function(input) {
     str_split_1("[^a-z0-9']+") %>%
     str_replace_all("^'?(.*?)'?$", "\\1") %>%
     discard(\(w) w == "") %>%
-    reduce(tally, .init = list())
+    enframe(name = NULL, value = "word") %>%
+    group_by(word) %>%
+    count() %>%
+    deframe() %>%
+    as.list()
 }
