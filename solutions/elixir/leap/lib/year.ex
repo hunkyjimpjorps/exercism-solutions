2c6ca83912d64ada9f8057133a8b98bc1b480460
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:56:15 +0000

    [Sync Iteration] elixir leap

--- solutions/elixir/leap/lib/year.ex
+++ solutions/elixir/leap/lib/year.ex
@@ -10,12 +10,11 @@ defmodule Year do
   """
   @spec leap_year?(non_neg_integer) :: boolean
   def leap_year?(year) do
-    remainders = [4, 100, 400] |> Enum.map(&(Integer.mod(year, &1)))
-    case remainders do
-      [_, _, 0] -> true
-      [_, 0, _] -> false
-      [0, _, _] -> true
-      _ -> false
-    end
+    divis? = &(evenly_divisible_by?(year, &1))
+    divis?.(4) && (!divis?.(100) || divis?.(400))
+  end
+
+  defp evenly_divisible_by?(dividend, divisor) do
+    Integer.mod(dividend, divisor) == 0
   end
 end
