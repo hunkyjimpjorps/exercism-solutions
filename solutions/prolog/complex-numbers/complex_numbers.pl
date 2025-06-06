48da401cb19e5e62c7b8751cf3f61e2da1661da9
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:44:25 +0000

    [Sync Iteration] prolog complex-numbers

---
+++ solutions/prolog/complex-numbers/complex_numbers.pl
@@ -0,0 +1,15 @@
+real((Re, _), Re).
+imaginary((_, Im), Im).
+
+conjugate((Re, Im), (Re, -1*Im)).
+inv((Re, Im), (-1*Re, -1*Im)).
+abs((Re, Im), (Re**2 + Im**2)**0.5).
+
+add((A, B), (C, D), (A + C, B + D)).
+sub((A, B), (C, D), (A - C, B - D)).
+
+mul((A, B), (C, D), (A * C - B * D, B * C + A * D)).
+div((A, B), (C, D), (RealPart, ImPart)) :-
+    Denominator is C^2 + D^2,
+    RealPart is (A * C + B * D)/Denominator, 
+    ImPart is (B * C - A * D)/Denominator.
