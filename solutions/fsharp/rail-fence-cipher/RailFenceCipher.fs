3771273ade8b654e1e83bf6cd0d70fe42b0f90c7
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:34 +0000

    [Sync Iteration] fsharp rail-fence-cipher

---
+++ solutions/fsharp/rail-fence-cipher/RailFenceCipher.fs
@@ -0,0 +1,22 @@
+﻿module RailFenceCipher
+
+let template rails =
+    seq {
+        while true do
+            yield! [| 0 .. rails - 1 |]
+            yield! [| rails - 2 .. -1 .. 1 |]
+    }
+
+let encode (rails: int) (message: string) =
+    Seq.zip (template rails) message
+    |> Seq.sortBy fst
+    |> Seq.map snd
+    |> System.String.Concat
+
+let decode rails (message: string) =
+    Seq.zip (template rails) [| 1 .. message.Length |]
+    |> Seq.sortBy fst
+    |> Seq.zip message
+    |> Seq.sortBy (snd >> snd)
+    |> Seq.map fst 
+    |> System.String.Concat
