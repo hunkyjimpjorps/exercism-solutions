c146b1f95ed2a7d38a6542de5d01f7adadc3dfa4
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:12 +0000

    [Sync Iteration] elixir killer-sudoku-helper

--- solutions/elixir/killer-sudoku-helper/lib/killer_sudoku_helper.ex
+++ solutions/elixir/killer-sudoku-helper/lib/killer_sudoku_helper.ex
@@ -5,9 +5,13 @@ defmodule KillerSudokuHelper do
   @spec combinations(cage :: %{exclude: [integer], size: integer, sum: integer}) :: [[integer]]
   def combinations(%{exclude: exclude, size: size, sum: sum}) do
     Enum.to_list(1..9) -- exclude
-    |> Combination.combine(size)
+    |> combine(size)
     |> Enum.map(&Enum.sort/1)
     |> Enum.sort_by(&hd/1)
     |> Enum.filter(&(Enum.sum(&1) == sum))
   end
+
+  defp combine(_, 0), do: [[]]
+  defp combine([], _), do: []
+  defp combine([h | t], n), do: (for rest <- combine(t, n - 1), do: [h | rest]) ++ combine(t, n)
 end
