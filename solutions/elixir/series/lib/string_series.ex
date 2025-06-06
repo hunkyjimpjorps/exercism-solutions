ee721b0bc132dd4ed8802ced7558ebb9c0b862d8
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:38 +0000

    [Sync Iteration] elixir series

---
+++ solutions/elixir/series/lib/string_series.ex
@@ -0,0 +1,16 @@
+defmodule StringSeries do
+  @doc """
+  Given a string `s` and a positive integer `size`, return all substrings
+  of that size. If `size` is greater than the length of `s`, or less than 1,
+  return an empty list.
+  """
+  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(_, size) when size <= 0, do: []
+
+  def slices(s, size) do
+    cond do
+      String.length(s) < size -> []
+      true -> [String.slice(s, 0, size) | slices(String.slice(s, 1..-1//1), size)]
+    end
+  end
+end
