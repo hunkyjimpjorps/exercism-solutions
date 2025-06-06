b1bcbf91d16a961cc1eb1ae3ac762f89ca73b28d
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:33 +0000

    [Sync Iteration] fsharp luhn

--- solutions/fsharp/luhn/Luhn.fs
+++ solutions/fsharp/luhn/Luhn.fs
@@ -3,19 +3,16 @@
 open System.Text.RegularExpressions
 
 let valid (number: string) =
-    if Regex.Match(number, "[^0-9\s]").Success
-       || Regex.Replace(number, " ", "").Length <= 1 then
-        false
-    else
-        number
-        |> fun n -> Regex.Replace(n, " ", "")
-        |> Seq.rev
-        |> Seq.map (string >> int)
-        |> Seq.indexed
-        |> Seq.sumBy
-            (function
-            | i, n when i % 2 = 0 -> n
-            | _, n when 2 * n > 9 -> 2 * n - 9
-            | _, n -> 2 * n)
-        |> (fun n -> n % 10)
-        |> (=) 0
+    not (Regex.IsMatch(number, "[^0-9\s]"))
+    && Regex.Replace(number, " ", "").Length > 1
+    && 0 = (number
+            |> fun n -> Regex.Replace(n, " ", "")
+            |> Seq.rev
+            |> Seq.map (string >> int)
+            |> Seq.indexed
+            |> Seq.sumBy
+                (function
+                | i, n when i % 2 = 0 -> n
+                | _, n when 2 * n > 9 -> 2 * n - 9
+                | _, n -> 2 * n)
+            |> (fun n -> n % 10))
