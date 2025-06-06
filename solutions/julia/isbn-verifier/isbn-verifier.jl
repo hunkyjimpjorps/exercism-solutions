c73387341beede44511af6882f4e2f12d00472fc
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:57 +0000

    [Sync Iteration] julia isbn-verifier

--- solutions/julia/isbn-verifier/isbn-verifier.jl
+++ solutions/julia/isbn-verifier/isbn-verifier.jl
@@ -1,29 +1,30 @@
-ISBN = Union{String}
 using Pipe:@pipe
 
-function isvalid(_::DataType, str::ISBN)::Bool
-    try
-        if isnothing(match(r"^\d{1}-*\d{3}-*\d{5}-*[0-9X]{1}$", str))
-            return false
+struct ISBN <: AbstractString
+    number::String
+    function ISBN(number)
+        if isvalid(ISBN, number)
+            replace(number, "-" => "") 
+        else
+            throw(DomainError(number, "Invalid ISBN-10 code"))
         end
-        @pipe replace(str, r"[^0-9X]" => "") |>
-            collect |>
-            map(c -> c == 'X' ? 10 : parse(Integer, c), _) |>
-            _ .* collect(10:-1:1) |>
-            sum |>
-            _ % 11 |>
-            ==(_, 0)
-    catch
-        throw(DomainError(str, "Invalid ISBN format"))
-    finally
-        false
     end
 end
 
-macro isbn_str(str::ISBN)
-    if isvalid(ISBN, str)
-        replace(str, r"[^0-9X]" => "")
-    else
-        throw(DomainError(str, "Invalid ISBN format"))
+function isvalid(_::DataType, str::String)::Bool
+    if isnothing(match(r"^\d{1}-*\d{3}-*\d{5}-*[0-9X]{1}$", str))
+        return false
     end
+    @pipe str |>
+        replace(_, "-" => "") |>
+        collect |>
+        map(c -> c == 'X' ? 10 : parse(Integer, c), _) |>
+        _ .* collect(10:-1:1) |>
+        sum |>
+        _ % 11 |>
+        ==(_, 0)
+end
+
+macro isbn_str(str::String)
+    ISBN(str)
 end
