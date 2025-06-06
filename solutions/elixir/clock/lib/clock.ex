f66bbf206cf54af6e7e65c028e16992e825ac7ed
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:49 +0000

    [Sync Iteration] elixir clock

--- solutions/elixir/clock/lib/clock.ex
+++ solutions/elixir/clock/lib/clock.ex
@@ -1,44 +1,22 @@
 defmodule Clock do
   defstruct hour: 0, minute: 0
+
   @doc """
   Returns a clock that can be represented as a string:
- 
+
       iex> Clock.new(8, 9) |> to_string
       "08:09"
   """
   @spec new(integer, integer) :: Clock
   def new(hour, minute) do
-    # do_new(hour, minute)
-    {hour, minute} = convert_to_mins(hour, minute) |> convert_mins_to_time()
+    hour = rem_e(rem_e(hour, 24) + div_e(minute, 60), 24)
+    minute = rem_e(minute, 60)
     %Clock{hour: hour, minute: minute}
   end
-  defp convert_to_mins(hour, minute) do
-    hour * 60 + minute
-  end
-  defp convert_mins_to_time(minutes) do
-    {hours, minutes} = div_with_rem(minutes, 60)
-    {_days, hour} = div_with_rem(hours, 24)
-    case minutes >= 0 do
-      true -> {rem(24 + hour, 24), minutes}
-      false -> {rem(24 + (hour - 1), 24), 60 + minutes}
-    end
-  end
-  defp div_with_rem(dividend, divisor) do
-    {div(dividend, divisor), rem(dividend, divisor)}
-  end
-  defimpl String.Chars, for: Clock do
-    def to_string(%Clock{} = clock) do
-      "#{pad(clock.hour)}:#{pad(clock.minute)}"
-    end
-    defp pad(integer) do
-      integer
-      |> Integer.to_string()
-      |> String.pad_leading(2, "0")
-    end
-  end
+
   @doc """
   Adds two clock times:
- 
+
       iex> Clock.new(10, 0) |> Clock.add(3) |> to_string
       "10:03"
   """
@@ -46,4 +24,24 @@ defmodule Clock do
   def add(%Clock{hour: hour, minute: minute}, add_minute) do
     new(hour, minute + add_minute)
   end
+
+  # Euclidean division and remainder to properly handle negative dividends
+  defp div_e(a, n) do
+    div(abs(n), n) * floor(a / abs(n))
+  end
+  defp rem_e(a, n) do
+    a - abs(n) * floor(a / abs(n))
+  end
+
+  defimpl String.Chars, for: Clock do
+    def to_string(clock) do
+      "#{format_part(clock.hour)}:#{format_part(clock.minute)}"
+    end
+
+    defp format_part(n) do
+      n
+      |> Integer.to_string()
+      |> String.pad_leading(2, "0")
+    end
+  end 
 end
