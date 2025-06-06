c08cfcd3f2dc48ecff450b02b8a8bdd26a24c3f8
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:32 +0000

    [Sync Iteration] fsharp prime-factors

---
+++ solutions/fsharp/prime-factors/PrimeFactors.fs
@@ -0,0 +1,11 @@
+﻿module PrimeFactors
+
+let factors (number: int64) : int list =
+
+    let rec nextFactor remainder candidate acc =
+        match candidate with
+        | _ when remainder = 1L -> acc
+        | n when remainder % n = 0L -> nextFactor (remainder / n) n (acc @ [ int n ])
+        | _ -> nextFactor remainder (candidate + 1L) acc
+
+    nextFactor number 2L List.empty
