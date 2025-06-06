0d757825ed37c2d58d57f0599a9692fd5e8ffc0b
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:28 +0000

    [Sync Iteration] fsharp kindergarten-garden

---
+++ solutions/fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,54 @@
+﻿module KindergartenGarden
+
+open FSharpx.Text
+
+type Plant =
+    | Clover
+    | Grass
+    | Radishes
+    | Violets
+    | Nothing
+
+let studentList =
+    [ "Alice"
+      "Bob"
+      "Charlie"
+      "David"
+      "Eve"
+      "Fred"
+      "Ginny"
+      "Harriet"
+      "Ileana"
+      "Joseph"
+      "Kincaid"
+      "Larry" ]
+
+let getPlantType (plant: char) : Plant =
+    match plant with
+    | 'C' -> Clover
+    | 'G' -> Grass
+    | 'R' -> Radishes
+    | 'V' -> Violets
+    | _ -> Nothing
+
+let divideGardenPlots (plots: string) : string list =
+    plots
+    |> Strings.split '\n'
+    |> Array.toList
+    |> List.map (Seq.chunkBySize 2)
+    |> List.map (Seq.toList)
+    |> List.map (List.map System.String)
+    |> (fun g -> List.map2 (fun a b -> a + b) g.[0] g.[1])
+
+let matchGardenPlotSeeds (plots: string) : Plant list =
+    let rec matchNext (plots: char list) : Plant list =
+        match plots with
+        | [] -> []
+        | head :: [] -> getPlantType head :: []
+        | head :: tail -> getPlantType head :: matchNext tail
+
+    matchNext (Seq.toList plots)
+
+let plants (diagram: string) (student: string) : Plant list =
+    let gardenPlots = divideGardenPlots diagram
+    matchGardenPlotSeeds gardenPlots.[List.findIndex (fun x -> x = student) studentList]
