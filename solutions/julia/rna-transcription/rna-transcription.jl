677a7ca39b87122b450c2aa7cca4e7c6edfc374d
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:53 +0000

    [Sync Iteration] julia rna-transcription

---
+++ solutions/julia/rna-transcription/rna-transcription.jl
@@ -0,0 +1,12 @@
+dna_to_rna = Dict('G' => 'C',
+                  'C' => 'G',
+                  'T' => 'A',
+                  'A' => 'U')
+
+function to_rna(dna)
+    try
+        string(collect(dna_to_rna[c] for c in dna)...)
+    catch 
+        throw(ErrorException("Invalid DNA base"))
+    end
+end
