80e19ee191a96dee9e143a682f687213bc1d7a7f
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:31 +0000

    [Sync Iteration] fsharp secret-handshake

---
+++ solutions/fsharp/secret-handshake/SecretHandshake.fs
@@ -0,0 +1,25 @@
+﻿module SecretHandshake
+
+open System.Collections
+
+let instructions =
+    [| "wink"
+       "double blink"
+       "close your eyes"
+       "jump" |]
+
+let commands (number: int) =
+    let bitsBA = BitArray([| number |])
+    bitsBA.Length <- 5
+    let bits = Array.create 5 true
+    bitsBA.CopyTo(bits, 0)
+
+    bits
+    |> Array.take 4
+    |> Array.zip instructions
+    |> Array.where snd
+    |> Array.map fst
+    |> Array.toList
+    |> match bits.[4] with
+        | true -> List.rev
+        | false -> id
