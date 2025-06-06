34acee790a89eec71960f155468625fcde3b5a64
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:55 +0000

    [Sync Iteration] julia raindrops

---
+++ solutions/julia/raindrops/raindrops.jl
@@ -0,0 +1,16 @@
+using OrderedCollections
+
+drops = OrderedDict(3 => "Pling", 5 => "Plang", 7 => "Plong")
+
+function raindrops(number)
+    result = ""
+    for dropfactor in keys(drops)
+        if number % dropfactor == 0
+            result = result * get(drops, dropfactor, "")
+        end
+    end
+    if result == ""
+        result = string(number)
+    end
+    return result
+end
