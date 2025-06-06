c5a7f4f7d3ca767f41e3236df5ba0505023a40df
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:55:59 +0000

    [Sync Iteration] prolog knapsack

--- solutions/prolog/knapsack/knapsack.pl
+++ solutions/prolog/knapsack/knapsack.pl
@@ -7,9 +7,7 @@ maximum_value(Items, Capacity, Value) :-
     maplist([I, W, V]>>(I = item(W, V)), Items, Weights, Values),
     same_length(Items, Chosen),
     Chosen ins 0..1,
-    maplist([C, W, V, CW, CV]>>(C * W #= CW, C * V #= CV), 
-        Chosen, Weights, Values, ChosenWeights, ChosenValues),
-    sum(ChosenWeights, #=<, Capacity),
-    sum(ChosenValues, #=, Value),
     sum(Chosen, #>, 0),
-    once(labeling([ff, max(Value)], Chosen)).
+    scalar_product(Weights, Chosen, #=<, Capacity),
+    scalar_product(Values, Chosen, #=, Value),
+    once(labeling([max(Value)], Chosen)).
