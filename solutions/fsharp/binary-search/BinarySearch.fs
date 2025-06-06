2b73b4e18d5eee2fadb5e13c213fd52807d3ed43
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:14 +0000

    [Sync Iteration] fsharp binary-search

--- solutions/fsharp/binary-search/BinarySearch.fs
+++ solutions/fsharp/binary-search/BinarySearch.fs
@@ -1,15 +1,20 @@
 ﻿module BinarySearch
 
 let find (input: int array) (value: int) : int option =
-    let rec findrec (input: (int * int) array)=
-        match input with
+    let rec findrec (subinput: (int * int) array) =
+        match subinput with
         | [||] -> None
         | _ ->
-            Array.item (input.Length / 2) input
+            Array.item (subinput.Length / 2) subinput
             |> snd
             |> function
-            | n when n = value -> Some(fst input.[input.Length / 2])
-            | n when n < value -> findrec input.[input.Length / 2 + 1..]
-            | _ -> findrec input.[..input.Length / 2 - 1]
+            | n when n = value -> Some(fst subinput.[subinput.Length / 2])
+            | n when n < value -> findrec subinput.[subinput.Length / 2 + 1..]
+            | _ -> findrec subinput.[..subinput.Length / 2 - 1]
 
-    findrec (Array.indexed input)
+    match value with
+    | n when
+        input = Array.empty
+        || n < Array.head input
+        || n > Array.last input -> None
+    | _ -> findrec (Array.indexed input)
