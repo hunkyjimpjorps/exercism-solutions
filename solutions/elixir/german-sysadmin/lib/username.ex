3bbf27d0be3a59a4005678fef986369fab014568
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:56 +0000

    [Sync Iteration] elixir german-sysadmin

--- solutions/elixir/german-sysadmin/lib/username.ex
+++ solutions/elixir/german-sysadmin/lib/username.ex
@@ -1,16 +1,17 @@
 defmodule Username do
   def sanitize(username) do
     username
-    |> Enum.map(fn x ->
-      case x do
-        ?ä -> 'ae'
-        ?ö -> 'oe'
-        ?ü -> 'ue'
-        ?ß -> 'ss'
-         _ -> x
+    |> Enum.filter(fn c -> c < 255 end)
+    |> to_string()
+    |> String.replace(["ä", "ö", "ü", "ß"], fn c ->
+      case c do
+        "ä" -> "ae"
+        "ö" -> "oe"
+        "ü" -> "ue"
+        "ß" -> "ss"
       end
     end)
-    |> List.flatten()
-    |> Enum.filter(&(&1 in Enum.concat([?_], ?a..?z)))
+    |> (&Regex.replace(~r/[^a-z_]/, &1, "")).()
+    |> to_charlist()
   end
 end
