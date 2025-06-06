6158f6e00257c388b1e1e0eec7994c2ea4abf2e5
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:10 +0000

    [Sync Iteration] fsharp hamming

--- solutions/fsharp/hamming/Hamming.fs
+++ solutions/fsharp/hamming/Hamming.fs
@@ -2,7 +2,7 @@
 
 let distance (strand1: string) (strand2: string) : int option =
     try
-        List.map2 (fun (s1: char) (s2: char) -> s1 <> s2) (strand1 |> List.ofSeq) (strand2 |> List.ofSeq)
+        (List.map2 (fun (s1: char) (s2: char) -> s1 <> s2) (strand1 |> List.ofSeq) (strand2 |> List.ofSeq))
         |> List.filter id
         |> List.length
         |> Some
