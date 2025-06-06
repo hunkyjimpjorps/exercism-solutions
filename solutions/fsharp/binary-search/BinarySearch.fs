649a8d2f8a47d136d1989eb8070610d1efcffab4
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:31 +0000

    [Sync Iteration] fsharp binary-search

---
+++ solutions/fsharp/binary-search/BinarySearch.fs
@@ -0,0 +1,19 @@
+﻿module BinarySearch
+
+let find (input: int array) (value: int) : int option =
+    let inputIndexed = Array.indexed input
+
+    let rec findrec (input: (int * int) array) value =
+        match input with
+        | [||] -> None
+        | [| n |] when (snd n) = value -> Some(fst n)
+        | _ ->
+            Array.item (input.Length / 2) input
+            |> snd
+            |> function
+            | n when n = value -> Some(fst input.[input.Length / 2])
+            | n when n < value -> findrec input.[input.Length / 2 + 1..] value
+            | n when n > value -> findrec input.[..input.Length / 2 - 1] value
+            | _ -> failwith "Comparison error"
+
+    findrec inputIndexed value
