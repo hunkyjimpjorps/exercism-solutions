0e51652869e6147be7e14083dab45f1642a243b2
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:32 +0000

    [Sync Iteration] fsharp atbash-cipher

---
+++ solutions/fsharp/atbash-cipher/AtbashCipher.fs
@@ -0,0 +1,29 @@
+﻿module AtbashCipher
+
+open System
+open System.Text.RegularExpressions
+
+let atbashMapping =
+    [| 'a' .. 'z' |]
+    |> fun a -> Array.zip a (Array.rev a)
+    |> Map.ofArray
+
+let atbashTransform =
+    String.map
+        (fun c ->
+            match Map.tryFind c atbashMapping with
+            | Some success -> success
+            | None -> c)
+
+let encode (str: string) =
+    str.ToLower()
+    |> fun s -> Regex.Replace(s, "[^a-z123]", "")
+    |> atbashTransform
+    |> Seq.chunkBySize 5
+    |> Seq.map String.Concat
+    |> fun s -> String.Join(' ', s)
+
+let decode str =
+    str
+    |> fun s -> Regex.Replace(s, "\s", "")
+    |> atbashTransform
