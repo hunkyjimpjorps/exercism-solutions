62a1861f4f516b9534b1950b8747fd2a4dd45dd6
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:29 +0000

    [Sync Iteration] fsharp rna-transcription

---
+++ solutions/fsharp/rna-transcription/RnaTranscription.fs
@@ -0,0 +1,14 @@
+﻿module RnaTranscription
+
+let toRna (dna: string) : string =
+    seq {
+        for b in dna do
+            yield
+                match b with
+                | 'G' -> 'C'
+                | 'C' -> 'G'
+                | 'T' -> 'A'
+                | 'A' -> 'U'
+                | _ -> failwith "Invalid base"
+    }
+    |> System.String.Concat
