70855dca2bff69841ec1f50c3f1f239733efdf1d
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:45 +0000

    [Sync Iteration] elixir binary-search-tree

---
+++ solutions/elixir/binary-search-tree/lib/binary_search_tree.ex
@@ -0,0 +1,34 @@
+defmodule BinarySearchTree do
+  @type bst_node :: %{data: any, left: bst_node | nil, right: bst_node | nil}
+
+  @doc """
+  Create a new Binary Search Tree with root's value as the given 'data'
+  """
+  @spec new(any) :: bst_node
+  def new(data) do
+    %{data: data, left: nil, right: nil}
+  end
+
+  @doc """
+  Creates and inserts a node with its value as 'data' into the tree.
+  """
+  @spec insert(bst_node, any) :: bst_node
+  def insert(tree = %{data: root, left: left_leaf, right: right_leaf}, data) do
+    cond do
+      data <= root && is_nil(left_leaf) -> %{tree | left: %{data: data, left: nil, right: nil}}
+      data <= root -> %{tree | left: insert(left_leaf, data)}
+      data > root && is_nil(right_leaf) -> %{tree | right: %{data: data, left: nil, right: nil}}
+      data > root -> %{tree | right: insert(right_leaf, data)}
+    end
+  end
+
+  @doc """
+  Traverses the Binary Search Tree in order and returns a list of each node's data.
+  """
+  @spec in_order(bst_node) :: [any]
+  def in_order(nil), do: []
+
+  def in_order(%{data: root, left: left_leaf, right: right_leaf}) do
+    in_order(left_leaf) ++ [root] ++ in_order(right_leaf)
+  end
+end
