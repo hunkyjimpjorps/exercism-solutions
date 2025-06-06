5c74df196e8009e72b35fc3f63b68fdae2b7fe9f
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:30 +0000

    [Sync Iteration] fsharp sieve

---
+++ solutions/fsharp/sieve/Sieve.fs
@@ -0,0 +1,9 @@
+﻿module Sieve
+
+let primes limit =
+    let rec nextSieve candidates acc =
+        match candidates with
+        | [] -> acc |> List.rev
+        | h :: t -> nextSieve (List.except [ 2 * h .. h .. limit ] t) (h :: acc)
+
+    nextSieve [ 2 .. limit ] List.empty
