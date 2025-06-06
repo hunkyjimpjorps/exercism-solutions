cb4ea0f00fd49d37de36241753425fb48e9d6394
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:56:08 +0000

    [Sync Iteration] elixir scrabble-score

--- solutions/elixir/scrabble-score/lib/scrabble.ex
+++ solutions/elixir/scrabble-score/lib/scrabble.ex
@@ -1,16 +1,15 @@
 defmodule Scrabble do
-  @values for {cs, p} <- %{
-                'AEIOULNRST' => 1,
-                'DG' => 2,
-                'BCMP' => 3,
-                'FHVWY' => 4,
-                'K' => 5,
-                'JX' => 8,
-                'QZ' => 10
-              },
-              c <- cs,
-              into: %{},
-              do: {c, p}
+  @values %{
+            'AEIOULNRST' => 1,
+            'DG' => 2,
+            'BCMP' => 3,
+            'FHVWY' => 4,
+            'K' => 5,
+            'JX' => 8,
+            'QZ' => 10
+          }
+          |> Enum.flat_map(fn {ks, v} -> Enum.map(ks, fn k -> {k, v} end) end)
+          |> Enum.into(%{})
   @doc """
   Calculate the scrabble score for the word.
   """
