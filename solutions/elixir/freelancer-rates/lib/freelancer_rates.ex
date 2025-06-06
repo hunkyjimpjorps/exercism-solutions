ec6cb2d9e859cc3b2a0c3659f752d17998336e85
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:53 +0000

    [Sync Iteration] elixir freelancer-rates

---
+++ solutions/elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -0,0 +1,17 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    hourly_rate * 8.0
+  end
+
+  def apply_discount(before_discount, discount) do
+    before_discount * (1 - discount / 100)
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    daily_rate(hourly_rate) * 22 * (1 - discount / 100) |> ceil
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    Float.floor(budget / (daily_rate(hourly_rate) * (1 - discount / 100)), 1)
+  end
+end
