7aa124fa460e9436589eaf7df4696181334bf873
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:29 +0000

    [Sync Iteration] fsharp nucleotide-count

---
+++ solutions/fsharp/nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,26 @@
+﻿module NucleotideCount
+
+open System.Text.RegularExpressions
+open FSharpPlus
+
+let (|HasInvalidNucleotides|_|) strand =
+    if Regex.Match(strand, "[^ACGT]").Success then
+        Some()
+    else
+        None
+
+let nucleotideCounts (strand: string) : Option<Map<char, int>> =
+    let emptyMap =
+        [ 'A'; 'C'; 'G'; 'T' ]
+        |> List.map (fun c -> (c, 0))
+        |> Map.ofList
+
+    match strand with
+    | HasInvalidNucleotides -> None
+    | s ->
+        s
+        |> Seq.toList
+        |> List.countBy id
+        |> Map.ofList
+        |> Map.unionWith (+) emptyMap
+        |> Some
