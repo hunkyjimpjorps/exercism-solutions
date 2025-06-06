b02d8d8de2b1b6a8f8ad53cd601b1586c464d0d3
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:35 +0000

    [Sync Iteration] fsharp high-scores

---
+++ solutions/fsharp/high-scores/HighScores.fs
@@ -0,0 +1,15 @@
+﻿module HighScores
+
+let scores (values: int list) : int list =
+    values
+
+let latest (values: int list) : int =
+    List.last values
+
+let personalBest (values: int list) : int =
+    List.max values 
+
+let personalTopThree (values: int list) : int list =
+    values
+    |> List.sortBy (~-)
+    |> List.truncate 3
