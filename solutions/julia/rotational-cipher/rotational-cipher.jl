4355d893550c85cfc245c65c6e253269aab5e02c
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:54 +0000

    [Sync Iteration] julia rotational-cipher

--- solutions/julia/rotational-cipher/rotational-cipher.jl
+++ solutions/julia/rotational-cipher/rotational-cipher.jl
@@ -1,13 +1,7 @@
 function rotate(n, str::String)
     shifted_str = ""
     for c in str
-        if isuppercase(Char(c))
-            shifted_str = string(shifted_str, rotate(n, c))
-        elseif islowercase(Char(c))
-            shifted_str = string(shifted_str, rotate(n, c))
-        else
-            shifted_str = string(shifted_str, c)
-        end        
+        shifted_str = string(shifted_str, rotate(n, c))  
     end
     shifted_str
 end
