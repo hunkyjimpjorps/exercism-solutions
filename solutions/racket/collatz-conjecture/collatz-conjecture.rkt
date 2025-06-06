a478d7aa8cb93733a68c051a8a4cf81036063e8a
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:44:08 +0000

    [Sync Iteration] racket collatz-conjecture

--- solutions/racket/collatz-conjecture/collatz-conjecture.rkt
+++ solutions/racket/collatz-conjecture/collatz-conjecture.rkt
@@ -2,5 +2,9 @@
 
 (provide collatz)
 
-(define (collatz num)
-  (error "Not implemented yet"))
+(define/contract (collatz n [acc 0])
+  (-> exact-positive-integer? natural?)
+  (cond [(= n 1) acc]
+        [(odd? n) (collatz (+ (* 3 n) 1) (add1 acc))]
+        [(even? n) (collatz (/ n 2) (add1 acc))]))
+
