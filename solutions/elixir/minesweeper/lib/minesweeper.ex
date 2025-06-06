724c1ae9becbfc5a498277722040c9b6d72247f2
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:48 +0000

    [Sync Iteration] elixir minesweeper

---
+++ solutions/elixir/minesweeper/lib/minesweeper.ex
@@ -0,0 +1,46 @@
+defmodule Minesweeper do
+  @doc """
+  Annotate empty spots next to mines with the number of mines next to them.
+  """
+  @spec annotate([String.t()]) :: [String.t()]
+
+  def annotate([]), do: []
+  def annotate([""]), do: [""]
+
+  def annotate(board) do
+    mine_positions = get_mine_positions(board)
+    cols = String.length(hd(board))
+
+    for pos <- Map.keys(mine_positions) |> Enum.sort(),
+        cell = Map.get(mine_positions, pos, " ") do
+      case cell do
+        "*" ->
+          "*"
+
+        " " ->
+          adjacent_cells(pos)
+          |> Enum.map(&Map.get(mine_positions, &1))
+          |> Enum.count(&(&1 == "*"))
+          |> then(&if &1 == 0, do: " ", else: &1)
+      end
+    end
+    |> Enum.chunk_every(cols)
+    |> Enum.map(&Enum.join/1)
+  end
+
+  def get_mine_positions(board) do
+    for {row, i} <- Enum.with_index(board),
+        {cell, j} <- Enum.with_index(String.codepoints(row)),
+        into: %{} do
+      {{i, j}, cell}
+    end
+  end
+
+  def adjacent_cells({i, j}) do
+    for delta_i <- -1..1,
+        delta_j <- -1..1,
+        {delta_i, delta_j} != {0, 0} do
+      {i + delta_i, j + delta_j}
+    end
+  end
+end
