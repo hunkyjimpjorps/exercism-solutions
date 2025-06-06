68535c22499233ba7933210d776e8454a5714c63
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:36 +0000

    [Sync Iteration] fsharp valentines-day

---
+++ solutions/fsharp/valentines-day/ValentinesDay.fs
@@ -0,0 +1,36 @@
+module ValentinesDay
+
+// TODO: please define the 'Approval' discriminated union type
+type Approval = 
+    | Yes
+    | No
+    | Maybe
+
+// TODO: please define the 'Cuisine' discriminated union type
+type Cuisine =
+    | Korean
+    | Turkish
+
+// TODO: please define the 'Genre' discriminated union type
+type Genre = 
+    | Crime
+    | Romance
+    | Horror
+    | Thriller
+
+// TODO: please define the 'Activity' discriminated union type
+type Activity =
+    | BoardGame
+    | Chill
+    | Movie of Genre
+    | Restaurant of Cuisine
+    | Walk of int
+
+let rateActivity (activity: Activity): Approval = 
+    match activity with
+    | Movie Genre.Romance -> Yes
+    | Restaurant Cuisine.Korean -> Yes 
+    | Walk distance when distance < 3 -> Yes
+    | Restaurant Cuisine.Turkish -> Maybe
+    | Walk distance when distance < 5 -> Maybe
+    | _ -> No
