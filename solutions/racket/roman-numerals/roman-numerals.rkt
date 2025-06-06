2079b001ccec61ea79fec841185ee7a3a7c1a7e4
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:15 +0000

    [Sync Iteration] racket roman-numerals

--- solutions/racket/roman-numerals/roman-numerals.rkt
+++ solutions/racket/roman-numerals/roman-numerals.rkt
@@ -18,12 +18,17 @@
 
 (define (to-roman n [conversions *to-roman*] [acc '()])
   (cond [(empty? conversions)
+         ; if the conversion list is empty, concatenate and return all the accumulated digits
          (apply string-append acc)]
         [(n . >= . (caar conversions))
+         ; if the top digit is less than the current amount,
+         ; subtract that digit's value from the current amount
+         ; and add that digit to the accumulator
          (to-roman (- n (caar conversions))
                    conversions
                    (append acc (list (cdar conversions))))]
         [else
+         ; otherwise, drop the top digit and continue 
          (to-roman n
                    (cdr conversions)
                    acc)]))
