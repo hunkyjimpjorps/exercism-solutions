c367e7c08ee664fc6bc9dd72cd5b2747f29ca64d
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:56:25 +0000

    [Sync Iteration] elixir crypto-square

--- solutions/elixir/crypto-square/lib/crypto_square.ex
+++ solutions/elixir/crypto-square/lib/crypto_square.ex
@@ -8,25 +8,27 @@ defmodule CryptoSquare do
   """
   @spec encode(String.t()) :: String.t()
   def encode(str) do
-    filtered_charlist =
+    filtered_str =
       str
       |> String.downcase()
       |> (&Regex.replace(~r/[^a-z0-9]/, &1, "")).()
       |> String.graphemes()
 
-    determine_dimensions(length(filtered_charlist), 1)
-    |> chunk_it(filtered_charlist)
+    {row, _} = determine_dimensions(length(filtered_str), 1)
+
+    Enum.chunk_every(
+      filtered_str,
+      row,
+      row,
+      List.duplicate(" ", row)
+    )
     |> List.zip()
     |> Enum.map(&Tuple.to_list/1)
     |> Enum.map(&Enum.join/1)
     |> Enum.join(" ")
   end
 
-  defp chunk_it(len, str) do
-    Enum.chunk_every(str, len, len, List.duplicate(" ", len))
-  end
-
-  defp determine_dimensions(n, c) when n <= c * (c - 1), do: c
-  defp determine_dimensions(n, c) when n <= c * c, do: c
+  defp determine_dimensions(n, c) when n <= c * (c - 1), do: {c, c - 1}
+  defp determine_dimensions(n, c) when n <= c * c, do: {c, c}
   defp determine_dimensions(n, c), do: determine_dimensions(n, c + 1)
 end
