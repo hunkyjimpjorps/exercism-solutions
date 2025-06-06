f878bdae66989c9ba0a78606d8df990cb59d121e
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:27 +0000

    [Sync Iteration] fsharp raindrops

---
+++ solutions/fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,7 @@
+﻿module Raindrops
+
+let convert (n: int) : string =
+    let drops = (if n % 3 = 0 then "Pling" else "") +
+                (if n % 5 = 0 then "Plang" else "") +
+                (if n % 7 = 0 then "Plong" else "")
+    if drops = "" then string n else drops
