f2e4c9327fabbae859eafc036a3097f5deb76685
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:03 +0000

    [Sync Iteration] elixir darts

--- solutions/elixir/darts/lib/darts.ex
+++ solutions/elixir/darts/lib/darts.ex
@@ -6,12 +6,11 @@ defmodule Darts do
   """
   @spec score(position :: position) :: integer
   def score({x, y}) do
-    d = Float.pow((x * x + y * y)/1, 0.5)
-    cond do
-      d <= 1 -> 10
-      d <= 5 -> 5
-      d <= 10 -> 1
-      true -> 0
+    case Float.pow((x * x + y * y)/1, 0.5) do
+      n when n <= 1 -> 10
+      n when n <= 5 -> 5
+      n when n <= 10 -> 1
+      _ -> 0
     end
   end
 end
