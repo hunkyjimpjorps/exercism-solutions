5c2b2432775d47847b1355a57ceb23d13b229f57
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:35 +0000

    [Sync Iteration] fsharp wordy

--- solutions/fsharp/wordy/Wordy.fs
+++ solutions/fsharp/wordy/Wordy.fs
@@ -8,33 +8,31 @@ type OperationTail = OperationNode list
 
 // parser primitives
 let ws = spaces
-
 let pBegin = skipString "What is"
 let pEnd = skipString "?"
 
 let pNumber = pint32
 
-let pOperator =
-    choice [ stringReturn "plus" (+)
-             stringReturn "minus" (-)
-             stringReturn "multiplied by" (*)
-             stringReturn "divided by" (/) ]
+let pOpParser =
+    new OperatorPrecedenceParser<int, unit, unit>()
 
-let pNextOp = (ws >>. pOperator) .>>. (ws >>. pNumber)
+pOpParser.TermParser <- pNumber .>> ws
+
+[| "plus", (+)
+   "minus", (-)
+   "multiplied by", (*)
+   "divided by", (/) |]
+|> Array.iter (fun (s, op) -> pOpParser.AddOperator(InfixOperator(s, ws, 1, Associativity.Left, op)))
 
 let pSentence =
-    tuple2 (pBegin .>> ws >>. pNumber) (many pNextOp .>> pEnd)
+    pBegin >>. ws >>. pOpParser.ExpressionParser .>> pEnd
 
 // parsing function
-
-let rec parseCaptures =
-    function
-    | acc, [] -> acc
-    | (acc: int), (op, x): OperationNode :: t ->
-        op acc x
-        |> (fun result -> parseCaptures (result, t))
-
 let answer (question: string) : int option =
     match run pSentence question with
-    | Success (result, _, _) -> Some(parseCaptures result)
-    | Failure (_) -> None
+    | Success (result, _, _) -> 
+        printfn $"Success: {question} was parsed as {result}"
+        Some(result)
+    | Failure (error, _, _) -> 
+        printfn $"Error: {error}"
+        None
