ca9bbe8192b714f28f540a3291136c7b06002652
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:33 +0000

    [Sync Iteration] fsharp crypto-square

---
+++ solutions/fsharp/crypto-square/CryptoSquare.fs
@@ -0,0 +1,25 @@
+﻿module CryptoSquare
+
+open System.Text.RegularExpressions
+
+let determineSize (n: int) : (int * int) =
+    let rec nextSize =
+        function
+        | n, c when n <= c * (c - 1) -> (c, c - 1)
+        | n, c when n <= c * c -> (c, c)
+        | n, c -> nextSize (n, c + 1)
+
+    nextSize (n, 1)
+
+let ciphertext (input: string) =
+    let padAndChunkString (str: string) =
+        let r, c = determineSize str.Length
+        str.PadRight(r * c) |> Seq.chunkBySize r
+
+    input.ToLower()
+    |> fun str -> Regex.Replace(str, "[^a-z0-9]", "")
+    |> padAndChunkString
+    |> Seq.toArray
+    |> Array.transpose
+    |> Array.map System.String.Concat
+    |> fun arr -> System.String.Join(' ', arr)
