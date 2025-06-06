15a21c222a5b3f961942d432ae53965ecb6bfce2
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:29 +0000

    [Sync Iteration] fsharp reverse-string

---
+++ solutions/fsharp/reverse-string/ReverseString.fs
@@ -0,0 +1,9 @@
+﻿module ReverseString
+
+let reverse (input: string) : string =
+    let rec reverseRecursive (input: char list) (acc: char list) : string =
+        match input with
+        | [] -> acc |> System.String.Concat
+        | h :: t -> reverseRecursive t (h :: acc)
+
+    reverseRecursive (input |> Seq.toList) List.empty
