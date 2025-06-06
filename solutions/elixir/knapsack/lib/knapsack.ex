63c9495087364524571ba554f63a0d54d9664814
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:08 +0000

    [Sync Iteration] elixir knapsack

--- solutions/elixir/knapsack/lib/knapsack.ex
+++ solutions/elixir/knapsack/lib/knapsack.ex
@@ -3,12 +3,16 @@ defmodule Knapsack do
   Return the maximum value that a knapsack can carry.
   """
 
-  defstruct [:weight_remaining, packed: [], current_value: 0]
-
-  @spec maximum_value(items :: [%{value: integer, weight: integer}], maximum_weight :: integer) ::
+  @spec maximum_value(
+          items :: [%{value: integer, weight: integer}],
+          maximum_weight :: integer
+        ) ::
           integer
   def maximum_value(items, max_weight) do
-    pack_knapsack(items, %Knapsack{weight_remaining: max_weight}).current_value
+    pack_knapsack(items, %{
+      current_value: 0,
+      weight_remaining: max_weight
+    }).current_value
   end
 
   defp pack_knapsack([], state), do: state
@@ -22,8 +26,7 @@ defmodule Knapsack do
     leave_it = pack_knapsack(rest, state)
 
     take_it =
-      pack_knapsack(rest, %Knapsack{
-        packed: [current_item | state.packed],
+      pack_knapsack(rest, %{
         current_value: state.current_value + current_item.value,
         weight_remaining: state.weight_remaining - current_item.weight
       })
