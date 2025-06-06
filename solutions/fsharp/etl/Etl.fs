baa975f97b0f5a88fd62c3114f86c268a356ff9d
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:29 +0000

    [Sync Iteration] fsharp etl

---
+++ solutions/fsharp/etl/Etl.fs
@@ -0,0 +1,14 @@
+﻿module Etl
+
+let rearrangeKey (k: int * char list) : (char * int) list =
+    let score, letterList = k
+
+    letterList
+    |> List.map (fun l -> (System.Char.ToLower l, score))
+
+let transform (scoresWithLetters: Map<int, char list>) =
+    scoresWithLetters
+    |> Map.toList
+    |> List.map rearrangeKey
+    |> List.concat
+    |> Map.ofList
