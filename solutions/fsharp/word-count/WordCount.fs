3945a6770c198ab95f90e55e1006f2df9132b88f
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:55:35 +0000

    [Sync Iteration] fsharp word-count

--- solutions/fsharp/word-count/WordCount.fs
+++ solutions/fsharp/word-count/WordCount.fs
@@ -4,10 +4,6 @@ open System.Text.RegularExpressions
 
 let countWords (phrase: string) =
     phrase.ToLower()
-    |> fun s -> Regex.Replace(s, "[^\w\s,']", "")
-    |> fun s -> Regex.Replace(s, "[\s,]+", "\n")
-    |> fun s -> Regex.Replace(s, "'(.+)'", "$1")
-    |> fun s -> s.Split([| '\n' |])
-    |> Seq.filter (fun s -> s.Equals("") |> not)
-    |> Seq.countBy id
+    |> fun s -> Regex.Matches(s, "(\w+(?:'\w+)?)")
+    |> Seq.countBy (fun m -> m.Value)
     |> Map.ofSeq
