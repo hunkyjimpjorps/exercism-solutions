f2e8bc932895194ebdae074b228339f788a57915
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:55:37 +0000

    [Sync Iteration] fsharp saddle-points

--- solutions/fsharp/saddle-points/SaddlePoints.fs
+++ solutions/fsharp/saddle-points/SaddlePoints.fs
@@ -1,12 +1,11 @@
 module SaddlePoints
 
-let saddlePoints (matrix: int list list) =
+let saddlePoints (mat: int list list) : (int * int) list =
 
     let m =
-        Array2D.initBased 1 1 matrix.Length matrix.[0].Length (fun i j -> matrix.[i - 1].[j - 1])
+        Array2D.initBased 1 1 mat.Length mat.[0].Length (fun i j -> mat.[i - 1].[j - 1])
 
-    m
-    |> Array2D.mapi (fun i j n -> (n, (i, j)))
+    Array2D.mapi (fun i j n -> (n, (i, j))) m
     |> Seq.cast
     |> Seq.filter (fun (n, (i, j)) -> n = Array.max m.[i, *] && n = Array.min m.[*, j])
     |> Seq.map snd
