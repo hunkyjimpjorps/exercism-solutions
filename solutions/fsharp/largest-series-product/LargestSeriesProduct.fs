6ce92b02e02484713c96481bff79198ca7776ca3
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:31 +0000

    [Sync Iteration] fsharp largest-series-product

---
+++ solutions/fsharp/largest-series-product/LargestSeriesProduct.fs
@@ -0,0 +1,26 @@
+﻿// Uses FsRegEx library.
+module LargestSeriesProduct
+
+let (|ValidNumber|) (input: string, _) : bool =
+    FsRegEx.isMatch @"^[0-9]*$" input
+
+let (|ZeroWindow|) (_, seriesLength) : bool = seriesLength = 0
+
+let (|InvalidWindow|) (input, seriesLength) : bool =
+    seriesLength > String.length input
+    || seriesLength < 0
+
+let (|EmptyString|) (input, _) : bool = input = ""
+
+let largestProduct input seriesLength : int option =
+    match (input, seriesLength) with
+    | ValidNumber false -> None
+    | InvalidWindow true -> None
+    | ZeroWindow true -> Some 1
+    | EmptyString true -> None
+    | _ ->
+        input
+        |> Seq.windowed seriesLength
+        |> Seq.map (Seq.fold (fun acc i -> acc * (string >> int) i) 1)
+        |> Seq.max
+        |> Some
