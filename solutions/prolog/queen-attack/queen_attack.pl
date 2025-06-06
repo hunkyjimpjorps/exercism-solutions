6075957db0de51b5c1e1decac5eb9a6181f9d771
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:27 +0000

    [Sync Iteration] prolog queen-attack

--- solutions/prolog/queen-attack/queen_attack.pl
+++ solutions/prolog/queen-attack/queen_attack.pl
@@ -2,7 +2,7 @@ create((Rank, File)) :-
     between(0, 7, Rank),
     between(0, 7, File).
 
-attack((R, _), (R, _)).
-attack((_, F), (_, F)).
+attack((R, _), (R, _)) :- true, !.
+attack((_, F), (_, F)) :- true, !.
 attack((R1, F1), (R2, F2)) :-
     abs(-(R1, R2)) =:= abs(-(F1, F2)).
