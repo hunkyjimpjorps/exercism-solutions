aa6d26f39c7aa86da5be5e4b2ed5b6bbc75b9360
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:38 +0000

    [Sync Iteration] elixir accumulate

---
+++ solutions/elixir/accumulate/lib/accumulate.ex
@@ -0,0 +1,24 @@
+defmodule Accumulate do
+  @doc """
+    Given a list and a function, apply the function to each list item and
+    replace it with the function's return value.
+
+    Returns a list.
+
+    ## Examples
+
+      iex> Accumulate.accumulate([], fn(x) -> x * 2 end)
+      []
+
+      iex> Accumulate.accumulate([1, 2, 3], fn(x) -> x * 2 end)
+      [2, 4, 6]
+
+  """
+
+  @spec accumulate(list, (any -> any)) :: list
+  def accumulate([], _f), do: []
+
+  def accumulate([head | tail], f) do
+    [f.(head) | accumulate(tail, f)]
+  end
+end
