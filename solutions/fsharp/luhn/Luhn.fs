86ab8b2cf150d19c78b9facd281d4ef1d8e54f20
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:17 +0000

    [Sync Iteration] fsharp luhn

--- solutions/fsharp/luhn/Luhn.fs
+++ solutions/fsharp/luhn/Luhn.fs
@@ -2,36 +2,20 @@
 
 open System.Text.RegularExpressions
 
-let makeActivePattern =
-    function
-    | true -> Some()
-    | false -> None
-
-let (|InvalidCharacter|_|) (n: string) =
-    Regex.IsMatch(n, "[^0-9]") |> makeActivePattern
-
-let (|TooShort|_|) (n: string) = n.Length <= 1 |> makeActivePattern
-
-let (|InvalidChecksum|_|) (n: string) =
-    n
-    |> fun n -> Regex.Replace(n, " ", "")
-    |> Seq.rev
-    |> Seq.map (string >> int)
-    |> Seq.indexed
-    |> Seq.sumBy
-        (function
-        | i, n when i % 2 = 0 -> n
-        | _, n when n >= 5 -> 2 * n - 9
-        | _, n -> 2 * n)
-    |> (fun n -> n % 10)
-    |> (<>) 0
-    |> makeActivePattern
-
-let valid (number: string) : bool =
-    number
-    |> fun n -> Regex.Replace(n, " ", "")
-    |> function
-        | InvalidCharacter
-        | TooShort
-        | InvalidChecksum -> false
-        | _ -> true
+let valid (number: string) =
+    if Regex.Match(number, "[^0-9\s]").Success
+       || Regex.Replace(number, " ", "").Length <= 1 then
+        false
+    else
+        number
+        |> fun n -> Regex.Replace(n, " ", "")
+        |> Seq.rev
+        |> Seq.map (string >> int)
+        |> Seq.indexed
+        |> Seq.sumBy
+            (function
+            | i, n when i % 2 = 0 -> n
+            | _, n when 2 * n > 9 -> 2 * n - 9
+            | _, n -> 2 * n)
+        |> (fun n -> n % 10)
+        |> (=) 0
