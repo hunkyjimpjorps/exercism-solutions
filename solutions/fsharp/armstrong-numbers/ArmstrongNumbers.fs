85ed6330bab4b17611fa61a47e773c8538d222c6
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:28 +0000

    [Sync Iteration] fsharp armstrong-numbers

---
+++ solutions/fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,14 @@
+﻿module ArmstrongNumbers
+
+let (|SumOfSquaresOfDigits|) (number: int) : bool =
+    let digits = string number
+
+    digits
+    |> Seq.fold (fun acc i -> acc + pown (int (string i)) (Seq.length digits)) 0
+    |> (=) number
+
+let isArmstrongNumber (number: int) : bool =
+    match number with
+    | n when n < 0 -> false
+    | SumOfSquaresOfDigits true -> true
+    | _ -> false
