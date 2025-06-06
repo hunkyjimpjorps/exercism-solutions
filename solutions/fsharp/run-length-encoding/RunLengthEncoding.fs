75fc9db3547bcb3698c6b32725d8a33e3b51c218
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:18 +0000

    [Sync Iteration] fsharp run-length-encoding

--- solutions/fsharp/run-length-encoding/RunLengthEncoding.fs
+++ solutions/fsharp/run-length-encoding/RunLengthEncoding.fs
@@ -1,12 +1,12 @@
 ﻿module RunLengthEncoding
 
 open System.Text.RegularExpressions
+open System
 
 let encodeOneChar c i =
-    if i = 1 then
-        string c
-    else
-        string i + string c
+    if i > 1 then $"{i}{c}"
+    else if i = 1 then $"{c}"
+    else ""
 
 let encode (input: string) =
     let rec encodeNext (input: char list) (encodeAcc: string) (charAcc: char) (countAcc: int) =
@@ -15,25 +15,19 @@ let encode (input: string) =
         | h :: t when h = charAcc -> encodeNext t encodeAcc charAcc (countAcc + 1)
         | h :: t -> encodeNext t $"{encodeAcc}{encodeOneChar charAcc countAcc}" h 1
 
-    if input = "" then
-        ""
-    else
-        encodeNext (Seq.toList input) "" (char input.[0]) 0
+    encodeNext (Seq.toList input) "" Char.MinValue 0
 
 let expandAbbreviation (abbrev: string) =
     if abbrev.Length = 1 then
         abbrev
     else
         Regex.Matches(abbrev, "(\d+)?(.)").[0].Groups
-        |> Seq.map (fun m -> m.Value)
         |> Seq.toList
+        |> List.map (fun m -> m.Value)
         |> (fun s -> List.replicate (int s.[1]) s.[2])
         |> System.String.Concat
 
 let decode input =
-    if input = "" then
-        ""
-    else
-        Regex.Matches(input, "(\d+)?[a-zA-Z\s]")
-        |> Seq.map ((fun m -> m.Value) >> expandAbbreviation)
-        |> System.String.Concat
+    Regex.Matches(input, "(\d+)?[a-zA-Z\s]")
+    |> Seq.map ((fun m -> m.Value) >> expandAbbreviation)
+    |> System.String.Concat
