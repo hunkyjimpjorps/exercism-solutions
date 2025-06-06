a6adb24f57eea0e41d396c8c56480fca391aaa70
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:53 +0000

    [Sync Iteration] elixir secrets

---
+++ solutions/elixir/secrets/lib/secrets.ex
@@ -0,0 +1,32 @@
+defmodule Secrets do
+  @spec secret_add(any) :: (number -> number)
+  def secret_add(secret) do
+    &(&1 + secret)
+  end
+
+  @spec secret_subtract(any) :: (number -> number)
+  def secret_subtract(secret) do
+    &(&1 - secret)
+  end
+
+  @spec secret_multiply(any) :: (number -> number)
+  def secret_multiply(secret) do
+    &(&1 * secret)
+  end
+
+  def secret_divide(secret) do
+    &(Integer.floor_div(&1, secret))
+  end
+
+  def secret_and(secret) do
+    &(Bitwise.&&&(&1, secret))
+  end
+
+  def secret_xor(secret) do
+    &(Bitwise.^^^(&1, secret))
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    fn n -> secret_function2.(secret_function1.(n)) end
+  end
+end
