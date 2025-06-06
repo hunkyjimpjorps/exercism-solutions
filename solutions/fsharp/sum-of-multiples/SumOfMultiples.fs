593b2a4da788b963c94d1a5603edfbe81b20cbdb
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:27 +0000

    [Sync Iteration] fsharp sum-of-multiples

---
+++ solutions/fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,12 @@
+﻿module SumOfMultiples
+
+let sum (numbers: int list) (upperBound: int) : int =
+    seq {
+        for n in numbers do
+            match n with
+            | 0 -> Seq.empty
+            | _ -> seq { n .. n .. upperBound - 1 }
+    }
+    |> Seq.concat
+    |> Seq.distinct
+    |> Seq.fold (+) 0
