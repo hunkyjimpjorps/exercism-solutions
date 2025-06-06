d9f3447d7cd2171ce4db7209f7a508ba6aa8fcb9
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:30 +0000

    [Sync Iteration] fsharp strain

---
+++ solutions/fsharp/strain/Strain.fs
@@ -0,0 +1,31 @@
+﻿module Seq
+
+let (|EmptySeq|FullSeq|) s =
+    match s with
+    | s when Seq.isEmpty s -> EmptySeq
+    | _ -> FullSeq((Seq.head s), (Seq.skip 1 s))
+
+let (|TruePredicate|_|) proc n =
+    match n with
+    | n when proc n -> Some n
+    | _ -> None
+
+let rec keep (pred: 'T -> bool) (xs: seq<'T>) : seq<'T> =
+    match xs with
+    | EmptySeq -> Seq.empty
+    | FullSeq (head, tail) ->
+        Seq.append
+            (match head with
+             | TruePredicate pred head -> [ head ]
+             | _ -> [])
+            (keep pred tail)
+
+let rec discard pred xs =
+    match xs with
+    | EmptySeq -> Seq.empty
+    | FullSeq (head, tail) ->
+        Seq.append
+            (match head with
+             | TruePredicate pred _ -> []
+             | head -> [ head ])
+            (discard pred tail)
