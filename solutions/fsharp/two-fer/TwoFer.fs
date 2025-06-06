75aad44b27d87e9c6c56fabc3a250779eeb2da02
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:55:26 +0000

    [Sync Iteration] fsharp two-fer

--- solutions/fsharp/two-fer/TwoFer.fs
+++ solutions/fsharp/two-fer/TwoFer.fs
@@ -1,8 +1,4 @@
 ﻿module TwoFer
 
 let twoFer (input: string option) : string =
-    let nameToUse =
-        match input with
-        | None -> "you"
-        | Some(input) -> input
-    sprintf "One for %s, one for me." nameToUse
+    $"""One for {Option.defaultValue "you" input}, one for me."""
