c0414d16a316b57999c3d0a3e93592b98a91896e
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:30 +0000

    [Sync Iteration] fsharp proverb

--- solutions/fsharp/proverb/Proverb.fs
+++ solutions/fsharp/proverb/Proverb.fs
@@ -9,10 +9,9 @@ let recite (input: string list) : string list =
 
     let rec reciteCycle (i: string list) (acc: string list) : string list =
         match i with
-        | one :: two :: rest when not rest.IsEmpty ->
-            reciteCycle (two :: rest) ($"For want of a {one} the {two} was lost." :: acc)
-        | _ :: two :: [] -> $"And all for the want of a {two}." :: acc
-        | one :: [] -> $"And all for the want of a {one}." :: acc
         | [] -> []
+        | one :: [] -> $"And all for the want of a {one}." :: acc
+        | _ :: two :: [] -> $"And all for the want of a {two}." :: acc
+        | one :: two :: rest -> reciteCycle (two :: rest) ($"For want of a {one} the {two} was lost." :: acc)
 
     reciteCycle inputCycle [] |> List.rev
