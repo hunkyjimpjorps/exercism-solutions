dcd33960ea9a20c002f5b8aab7977cdb8d835123
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:49 +0000

    [Sync Iteration] elixir poker

--- solutions/elixir/poker/lib/poker.ex
+++ solutions/elixir/poker/lib/poker.ex
@@ -89,11 +89,9 @@ defmodule Poker do
 
   defp compare_hands({type, []}, {type, []}), do: true
 
-  defp compare_hands({type, [a | rest_a]}, {type, [b | rest_b]}) do
-    if a == b do
-      compare_hands({type, rest_a}, {type, rest_b})
-    else
-      a >= b
-    end
-  end
+  defp compare_hands({type, [n | rest1]}, {type, [n | rest2]}), 
+    do: compare_hands({type, rest1}, {type, rest2})
+
+  defp compare_hands({type, [n1 | _]}, {type, [n2 | _]}),
+    do: n1 >= n2
 end
