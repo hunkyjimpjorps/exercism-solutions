a12b6740e9103072a20861ef979b71e4bb7b1e5d
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:13 +0000

    [Sync Iteration] fsharp perfect-numbers

--- solutions/fsharp/perfect-numbers/PerfectNumbers.fs
+++ solutions/fsharp/perfect-numbers/PerfectNumbers.fs
@@ -8,8 +8,8 @@ type Classification =
 let (|IsGreaterThan|IsEqualTo|IsLessThan|IsInvalid|) (a, b) =
     match a with
     | _ when b <= 0 -> IsInvalid
-    | n when n > b -> IsGreaterThan
-    | n when n < b -> IsLessThan
+    | _ when a > b -> IsGreaterThan
+    | _ when a < b -> IsLessThan
     | _ -> IsEqualTo
 
 let aliquotSum (n: int) =
