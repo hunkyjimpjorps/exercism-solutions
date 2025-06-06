27002ea32b7ac3ff9d3fe864c9cc872e403b6e46
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:28 +0000

    [Sync Iteration] fsharp raindrops

--- solutions/fsharp/raindrops/Raindrops.fs
+++ solutions/fsharp/raindrops/Raindrops.fs
@@ -3,14 +3,14 @@
 type divisor = int * string
 
 let convert (n: int) : string =
-    let divisorSounds: divisor list = [ 3, "Pling"; 5, "Plang"; 7, "Plong" ]
+    let divisorSounds : divisor list = [ 3, "Pling"; 5, "Plang"; 7, "Plong" ]
 
-    let isDivisible (div, sound) =
+    let isDivisible ((div, sound):divisor) : string option =
         match n % div with
         | 0 -> Some sound
         | _ -> None
 
-    let drops =
+    let drops : string =
         List.choose isDivisible divisorSounds
         |> String.concat ""
 
