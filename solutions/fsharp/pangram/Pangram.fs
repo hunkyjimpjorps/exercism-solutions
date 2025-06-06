cd998363dd9a74c3c7782ba720a99a1749e44098
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:29 +0000

    [Sync Iteration] fsharp pangram

---
+++ solutions/fsharp/pangram/Pangram.fs
@@ -0,0 +1,8 @@
+﻿module Pangram
+
+let isPangram (input: string) : bool =
+    String.filter (fun c -> System.Char.IsLetter c) input
+    |> String.map (fun c -> System.Char.ToLower c)
+    |> Seq.toList
+    |> List.distinct
+    |> List.length = 26
