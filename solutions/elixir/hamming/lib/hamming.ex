83eb1ac017b51bd3b2340a91d5e37f62d0fd3e3c
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:41 +0000

    [Sync Iteration] elixir hamming

---
+++ solutions/elixir/hamming/lib/hamming.ex
@@ -0,0 +1,22 @@
+defmodule Hamming do
+  @doc """
+  Returns number of differences between two strands of DNA, known as the Hamming Distance.
+
+  ## Examples
+
+  iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
+  {:ok, 4}
+  """
+  @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
+  def hamming_distance(strand1, strand2) do
+    case {length(strand1), length(strand2)} do
+      {l, l} ->
+        {:ok,
+         Enum.zip_with(strand1, strand2, fn s1, s2 -> s1 != s2 end)
+         |> Enum.count(&Function.identity/1)}
+
+      _ ->
+        {:error, "strands must be of equal length"}
+    end
+  end
+end
