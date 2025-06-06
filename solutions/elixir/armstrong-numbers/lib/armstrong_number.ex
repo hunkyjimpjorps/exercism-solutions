1237d1befd47ec131b77273e14e8b7a34cc10bc0
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:56:35 +0000

    [Sync Iteration] elixir armstrong-numbers

--- solutions/elixir/armstrong-numbers/lib/armstrong_number.ex
+++ solutions/elixir/armstrong-numbers/lib/armstrong_number.ex
@@ -2,13 +2,10 @@ defmodule ArmstrongNumber do
   @moduledoc """
   Provides a way to validate whether or not a number is an Armstrong number
   """
-  defp digits(0), do: []
-  defp digits(n), do: [Integer.mod(n, 10) | digits(div(n, 10))]
-
   defp armstrong_sum(ns), do: Enum.reduce(ns, 0, fn n, acc -> acc + Integer.pow(n, length(ns)) end)
 
   @spec valid?(integer) :: boolean
   def valid?(number) do
-    number |> digits() |> armstrong_sum() |> (&(&1 == number)).()
+    number |> Integer.digits() |> armstrong_sum() |> (&(&1 == number)).()
   end
 end
