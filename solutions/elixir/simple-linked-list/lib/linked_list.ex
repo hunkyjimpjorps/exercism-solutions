f36b2a47d05035eb9aaabf07f6493093f47c56e0
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:34 +0000

    [Sync Iteration] elixir simple-linked-list

--- solutions/elixir/simple-linked-list/lib/linked_list.ex
+++ solutions/elixir/simple-linked-list/lib/linked_list.ex
@@ -1,12 +1,13 @@
 defmodule LinkedList do
-  @type cons_pair :: {any, cons_pair} | {nil}
+  @opaque t :: tuple()
+  @type cons_pair :: {any, nil | cons_pair}
 
   @empty {:error, :empty_list}
 
   @doc """
   Construct a new LinkedList
   """
-  @spec new() :: cons_pair
+  @spec new() :: t
   def new() do
     {nil}
   end
@@ -14,7 +15,7 @@ defmodule LinkedList do
   @doc """
   Push an item onto a LinkedList
   """
-  @spec push(cons_pair, any()) :: cons_pair
+  @spec push(t, any()) :: t
   def push(list, elem) do
     {elem, list}
   end
@@ -22,57 +23,57 @@ defmodule LinkedList do
   @doc """
   Counts the number of elements in a LinkedList
   """
-  @spec count(cons_pair) :: non_neg_integer()
+  @spec count(t) :: non_neg_integer()
   def count({nil}), do: 0
   def count({_, tail}), do: 1 + count(tail)
 
   @doc """
   Determine if a LinkedList is empty
   """
-  @spec empty?(cons_pair) :: boolean()
+  @spec empty?(t) :: boolean()
   def empty?({nil}), do: true
   def empty?({_, _}), do: false
 
   @doc """
   Get the value of a head of the LinkedList
   """
-  @spec peek(cons_pair) :: {:ok, any()} | {:error, :empty_list}
+  @spec peek(t) :: {:ok, any()} | {:error, :empty_list}
   def peek({nil}), do: @empty
   def peek({head, _}), do: {:ok, head}
 
   @doc """
   Get tail of a LinkedList
   """
-  @spec tail(cons_pair) :: {:ok, cons_pair} | {:error, :empty_list}
+  @spec tail(t) :: {:ok, t} | {:error, :empty_list}
   def tail({nil}), do: @empty
   def tail({_, tail}), do: {:ok, tail}
 
   @doc """
   Remove the head from a LinkedList
   """
-  @spec pop(cons_pair) :: {:ok, any(), cons_pair} | {:error, :empty_list}
+  @spec pop(t) :: {:ok, any(), t} | {:error, :empty_list}
   def pop({nil}), do: @empty
   def pop({head, tail}), do: {:ok, head, tail}
 
   @doc """
   Construct a LinkedList from a stdlib List
   """
-  @spec from_list(list()) :: cons_pair
+  @spec from_list(list()) :: t
   def from_list([]), do: {nil}
-  def from_list([head | tail]), do: push(from_list(tail), head)
+  def from_list([head | tail]), do: {head, from_list(tail)}
 
   @doc """
   Construct a stdlib List LinkedList from a LinkedList
   """
-  @spec to_list(cons_pair) :: list()
+  @spec to_list(t) :: list()
   def to_list({nil}), do: []
   def to_list({head, tail}), do: [head | to_list(tail)]
 
   @doc """
   Reverse a LinkedList
   """
-  @spec reverse(cons_pair) :: cons_pair
+  @spec reverse(t) :: t
   def reverse(list), do: do_reverse(list, {nil})
   defp do_reverse({nil}, acc), do: acc
-  defp do_reverse({head, tail}, acc), do: do_reverse(tail, push(acc, head))
+  defp do_reverse({head, tail}, acc), do: do_reverse(tail, {head, acc})
 end
