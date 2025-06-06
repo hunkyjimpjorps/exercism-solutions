c63123e96c4d1c2404e80a08f8c82e89955ba3f4
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:35 +0000

    [Sync Iteration] fsharp dnd-character

---
+++ solutions/fsharp/dnd-character/DndCharacter.fs
@@ -0,0 +1,34 @@
+﻿module DndCharacter
+
+open FSharpPlus.Math.Generic
+open System
+
+let r = Random()
+
+type Character =
+    { Strength: int
+      Dexterity: int
+      Constitution: int
+      Intelligence: int
+      Wisdom: int
+      Charisma: int
+      Hitpoints: int }
+
+let modifier x = divE (x - 10) 2
+
+let ability () =
+    [for _ in 1 .. 4 do yield r.Next(1, 7) ]
+    |> List.sort
+    |> List.tail
+    |> List.sum
+
+let createCharacter () : Character =
+    let con = ability ()
+
+    { Strength = ability ()
+      Dexterity = ability ()
+      Constitution = con
+      Intelligence = ability ()
+      Wisdom = ability ()
+      Charisma = ability ()
+      Hitpoints = 10 + modifier con }
