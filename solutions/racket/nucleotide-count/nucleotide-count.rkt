4c8dfc5085de6a1a258b58d80a58f2f506b5db99
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:44:09 +0000

    [Sync Iteration] racket nucleotide-count

--- solutions/racket/nucleotide-count/nucleotide-count.rkt
+++ solutions/racket/nucleotide-count/nucleotide-count.rkt
@@ -2,5 +2,11 @@
 
 (provide nucleotide-counts)
 
-(define (nucleotide-counts nucleotides)
-  (error "Not implemented yet"))
+(define (nucleotide-counts strand)
+  (define nucleotides-hash (make-hash '((#\A . 0)
+                                        (#\C . 0)
+                                        (#\G . 0)
+                                        (#\T . 0))))
+  (for ([nucleotide [in-string strand]])
+    (hash-update! nucleotides-hash nucleotide add1))
+  (sort (hash->list nucleotides-hash) char<? #:key car))
