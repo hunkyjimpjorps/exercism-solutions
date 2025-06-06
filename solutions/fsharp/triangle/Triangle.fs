223f975b2cdf1b8c4752cb03598103776056b85e
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:29 +0000

    [Sync Iteration] fsharp triangle

---
+++ solutions/fsharp/triangle/Triangle.fs
@@ -0,0 +1,20 @@
+﻿module Triangle
+
+let uniqueSides (triangle: float list) =
+    triangle |> List.distinct |> List.length
+
+let triangleInequality triangle =
+    triangle 
+    |> List.sort 
+    |> function 
+        | [a; b; c] when a + b > c -> true
+        | _ -> false
+
+let equilateral triangle =
+    uniqueSides triangle = 1 && triangleInequality triangle
+
+let isosceles triangle =
+    uniqueSides triangle <= 2 && triangleInequality triangle
+
+let scalene triangle =
+    uniqueSides triangle = 3 && triangleInequality triangle
