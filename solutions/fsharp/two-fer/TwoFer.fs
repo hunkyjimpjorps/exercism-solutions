99c685b9f7a37d23497ec9cfa9bf483145093884
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:27 +0000

    [Sync Iteration] fsharp two-fer

---
+++ solutions/fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,8 @@
+﻿module TwoFer
+
+let twoFer (input: string option) : string =
+    let nameToUse =
+        match input with
+        | None -> "you"
+        | Some(input) -> input
+    sprintf "One for %s, one for me." nameToUse
