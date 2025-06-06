1d1eed41ef7db8b9a4b6ae089d67bb29fc908ee0
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:46 +0000

    [Sync Iteration] elixir perfect-numbers

---
+++ solutions/elixir/perfect-numbers/lib/perfect_numbers.ex
@@ -0,0 +1,27 @@
+defmodule PerfectNumbers do
+  @doc """
+  Determine the aliquot sum of the given `number`, by summing all the factors
+  of `number`, aside from `number` itself.
+
+  Based on this sum, classify the number as:
+
+  :perfect if the aliquot sum is equal to `number`
+  :abundant if the aliquot sum is greater than `number`
+  :deficient if the aliquot sum is less than `number`
+  """
+  defp factors(number) do
+    Enum.filter(Enum.to_list(1..floor(:math.sqrt(number))), fn f -> Integer.mod(number, f) == 0 end)
+    |> Enum.flat_map(fn n -> if(div(number, n) == n, do: [n], else: [n, div(number, n)]) end)
+    |> Enum.sum()
+  end
+
+  @spec classify(number :: integer) :: {:ok, atom} | {:error, String.t()}
+  def classify(number) when number < 1, do: {:error, "Classification is only possible for natural numbers."}
+  def classify(number) do
+    case factors(number) do
+      n when n == 2 * number -> {:ok, :perfect}
+      n when n > 2 * number -> {:ok, :abundant}
+      n when n < 2 * number -> {:ok, :deficient}      
+    end
+  end
+end
