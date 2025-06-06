1ca310704b1f3b1a4e1e8edf23d3e290da5e2949
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:49 +0000

    [Sync Iteration] elixir dot-dsl

--- solutions/elixir/dot-dsl/lib/dot.ex
+++ solutions/elixir/dot-dsl/lib/dot.ex
@@ -1,21 +1,4 @@
 defmodule Dot do
-  defmacro graph(do: ast) do
-    with {_, graph} = Macro.prewalk(ast, Graph.new(), &walk_graph/2), do: Macro.escape(graph)
+  defmacro graph(ast) do
   end
-
-  defp walk_graph({:graph, _, [attrs]}, graph), do: {{}, Graph.put_attrs(graph, attrs)}
-  defp walk_graph({node, _, nil}, graph), do: {{}, Graph.add_node(graph, node)}
-
-  defp walk_graph({node, _, [attrs]}, graph) when is_list(attrs),
-    do: {{}, Graph.add_node(graph, node, attrs)}
-
-  defp walk_graph({:--, _, [{from, _, _}, {to, _, nil}]}, graph),
-    do: {{}, Graph.add_edge(graph, from, to)}
-
-  defp walk_graph({:--, _, [{from, _, _}, {to, _, [attrs]}]}, graph) when is_list(attrs),
-    do: {{}, Graph.add_edge(graph, from, to, attrs)}
-
-  defp walk_graph({_, _, _} = ast, graph), do: {ast, graph}
-
-  defp walk_graph(_, _), do: raise(ArgumentError)
 end
