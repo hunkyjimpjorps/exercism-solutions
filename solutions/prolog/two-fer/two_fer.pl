3fe33d6a240e70ce1f18a0cff8450169587d7271
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:55:59 +0000

    [Sync Iteration] prolog two-fer

--- solutions/prolog/two-fer/two_fer.pl
+++ solutions/prolog/two-fer/two_fer.pl
@@ -1,5 +1,5 @@
 two_fer(Name, Dialogue) :-
-    format(string(Dialogue), 'One for ~s, one for me.', [Name]).
+    swritef(Dialogue, "One for %w, one for me.", [Name]).
 
 two_fer(Dialogue) :-
     two_fer("you", Dialogue).
