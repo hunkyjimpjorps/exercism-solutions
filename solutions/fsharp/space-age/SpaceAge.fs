19f5df5eb83311259434efc4fa4ea4f55d3595d6
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:27 +0000

    [Sync Iteration] fsharp space-age

---
+++ solutions/fsharp/space-age/SpaceAge.fs
@@ -0,0 +1,21 @@
+﻿module SpaceAge
+
+type Planet =
+    | Earth  | Mercury | Venus
+    | Mars   | Jupiter | Saturn
+    | Uranus | Neptune 
+    
+let earthYearLength = 31557600.0
+    
+let age (planet: Planet) (seconds: int64) : float =
+    let conversionFactor =
+        match planet with
+            | Earth   -> 1.0
+            | Mercury -> 0.2408467
+            | Venus   -> 0.61519726
+            | Mars    -> 1.8808158
+            | Jupiter -> 11.862615
+            | Saturn  -> 29.447498
+            | Uranus  -> 84.016846
+            | Neptune -> 164.79132
+    (float)seconds / earthYearLength / conversionFactor
