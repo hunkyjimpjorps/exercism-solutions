28df72f448482f85e9d3d0d65e9c84fe10a858fb
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:44:25 +0000

    [Sync Iteration] prolog triangle

---
+++ solutions/prolog/triangle/triangle.pl
@@ -0,0 +1,15 @@
+unique_sides(Sides, N) :- 
+    \+ member(0, Sides),
+    sort(0, @=<, Sides, [Short1, Short2, Long]),
+    Short1 + Short2 >= Long,
+    list_to_set(Sides, UniqueSides),
+    length(UniqueSides, N).
+
+classify_triangle(1, "equilateral").
+classify_triangle(1, "isosceles").
+classify_triangle(2, "isosceles").
+classify_triangle(3, "scalene").    
+
+triangle(A, B, C, Type) :- 
+    unique_sides([A, B, C], N),
+    once(classify_triangle(N, Type)).
