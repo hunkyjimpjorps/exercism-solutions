6ef9c0de7ac06ba900208297424cff07095335d0
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:32 +0000

    [Sync Iteration] fsharp parallel-letter-frequency

---
+++ solutions/fsharp/parallel-letter-frequency/ParallelLetterFrequency.fs
@@ -0,0 +1,20 @@
+﻿module ParallelLetterFrequency
+
+open FSharpPlus
+open System.Text.RegularExpressions
+
+let asyncParse (text: string) =
+    async {
+        return
+            text.ToLower()
+            |> (fun t -> Regex.Replace(t, "[\s.,'0-9]", ""))
+            |> Seq.countBy id
+    }
+
+let frequency (texts: string list) =
+    texts
+    |> Seq.map asyncParse
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Seq.map Map.ofSeq
+    |> Seq.fold (fun acc m -> Map.unionWith (+) acc m) Map.empty
