b1bf17e9cb1813c1f85c9f83ddb5520d53cdf06a
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:56:19 +0000

    [Sync Iteration] elixir gigasecond

--- solutions/elixir/gigasecond/lib/gigasecond.ex
+++ solutions/elixir/gigasecond/lib/gigasecond.ex
@@ -4,10 +4,12 @@ defmodule Gigasecond do
   """
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
           {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
-  def from(erl_datetime) do
-    with {:ok, start} <- NaiveDateTime.from_erl(erl_datetime),
-         future <- NaiveDateTime.add(start, 1_000_000_000),
-         erl_future <- NaiveDateTime.to_erl(future),
-         do: erl_future
+  def from({{year, month, day}, {hours, minutes, seconds}}) do
+    with {:ok, date} <- Date.new(year, month, day),
+         {:ok, time} <- Time.new(hours, minutes, seconds),
+         {:ok, datetime} <- DateTime.new(date, time),
+         future <- DateTime.add(datetime, 1_000_000_000) do
+      {{future.year, future.month, future.day}, {future.hour, future.minute, future.second}}
+    end
   end
 end
