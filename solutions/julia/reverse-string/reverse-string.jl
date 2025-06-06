e58365fe1877eb85f4a8efdc551cdb7442bd01ac
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:58 +0000

    [Sync Iteration] julia reverse-string

--- solutions/julia/reverse-string/reverse-string.jl
+++ solutions/julia/reverse-string/reverse-string.jl
@@ -1,5 +1,8 @@
 using Unicode
+const TEST_GRAPHEMES = true
 
 function myreverse(str::String)
-    [c for c in Unicode.graphemes(str)] |> reverse
+    [c for c in Unicode.graphemes(str)] |> 
+    reverse |>
+    join
 end
