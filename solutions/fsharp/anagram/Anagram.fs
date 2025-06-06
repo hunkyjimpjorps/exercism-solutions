f205a565aa5745c98d222c74079aabd63266bacb
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:31 +0000

    [Sync Iteration] fsharp anagram

---
+++ solutions/fsharp/anagram/Anagram.fs
@@ -0,0 +1,15 @@
+﻿module Anagram
+
+open FSharpPlus
+
+let sortChars =
+    (fun (s: string) -> s.ToLower())
+    >> Seq.toArray
+    >> Array.sort
+
+let checkAnagram (mainWord: string) (candidate: string) =
+    sortChars candidate = sortChars mainWord
+    && candidate.ToLower() <> mainWord.ToLower()
+
+let findAnagrams candidates mainWord =
+    List.filter (checkAnagram mainWord) candidates
