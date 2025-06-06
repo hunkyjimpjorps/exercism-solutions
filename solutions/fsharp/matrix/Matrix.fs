f702bbbbeee70cafcce4a805495ec047fd7d972f
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:31 +0000

    [Sync Iteration] fsharp matrix

---
+++ solutions/fsharp/matrix/Matrix.fs
@@ -0,0 +1,19 @@
+﻿module Matrix
+
+open FSharpx 
+
+let stringToMatrix (str: string) : int [,] =
+    str
+    |> String.splitChar [| '\n' |]
+    |> Array.map (String.splitChar [| ' ' |] >> Array.map int)
+    |> array2D
+
+let row index matrix =
+    stringToMatrix matrix
+    |> fun m -> m.[index-1, *]
+    |> Array.toList
+
+let column index matrix =
+    stringToMatrix matrix
+    |> fun m -> m.[*, index-1]
+    |> Array.toList
