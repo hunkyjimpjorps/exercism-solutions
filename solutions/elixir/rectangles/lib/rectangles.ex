037255e9dc8330bbfa4085e52d1b4173a0e7eefb
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:09 +0000

    [Sync Iteration] elixir rectangles

--- solutions/elixir/rectangles/lib/rectangles.ex
+++ solutions/elixir/rectangles/lib/rectangles.ex
@@ -11,8 +11,8 @@ defmodule Rectangles do
     for p1 <- vertices,
         p2 <- vertices,
         p2.r > p1.r and p1.c > p2.c,
-        matrix[%{r: p1.r, c: p2.c}] == ?+,
-        matrix[%{r: p2.r, c: p1.c}] == ?+,
+        matrix[pt(p1.r, p2.c)] == ?+,
+        matrix[pt(p2.r, p1.c)] == ?+,
         all_connected?(matrix, p1, p2),
         reduce: 0 do
       acc -> acc + 1
@@ -23,16 +23,18 @@ defmodule Rectangles do
     for {row, r} <- Enum.with_index(String.split(str, "\n")),
         {char, c} <- Enum.with_index(String.to_charlist(row)),
         into: %{} do
-      {%{r: r, c: c}, char}
+      {pt(r, c), char}
     end
   end
 
-  defp all_connected?(matrix, p1, p2) do
+  defp all_connected?(m, p1, p2) do
     Enum.all?([
-      Enum.all?(p1.r..p2.r, &(matrix[%{r: &1, c: p1.c}] in '+|')),
-      Enum.all?(p1.r..p2.r, &(matrix[%{r: &1, c: p2.c}] in '+|')),
-      Enum.all?(p1.c..p2.c, &(matrix[%{r: p1.r, c: &1}] in '+-')),
-      Enum.all?(p1.c..p2.c, &(matrix[%{r: p2.r, c: &1}] in '+-'))
+      Enum.all?(p1.r..p2.r, &(m[pt(&1, p1.c)] in '+|')),
+      Enum.all?(p1.r..p2.r, &(m[pt(&1, p2.c)] in '+|')),
+      Enum.all?(p1.c..p2.c, &(m[pt(p1.r, &1)] in '+-')),
+      Enum.all?(p1.c..p2.c, &(m[pt(p2.r, &1)] in '+-'))
     ])
   end
+
+  defp pt(r, c), do: %{r: r, c: c}
 end
