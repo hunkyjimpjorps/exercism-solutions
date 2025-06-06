8e4ec411afdfb7beb7fefebd80c71e65f0baefec
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:56 +0000

    [Sync Iteration] julia sieve

--- solutions/julia/sieve/sieve.jl
+++ solutions/julia/sieve/sieve.jl
@@ -1,7 +1,7 @@
 function sieve(limit)
     sieveNumbers = 2:limit
     for i in 2:limit
-        sieveNumbers = filter(!(in(collect(2i:i:limit))), sieveNumbers)
+        sieveNumbers = filter(!in(collect(2i:i:limit)), sieveNumbers)
     end
     sieveNumbers
 end
