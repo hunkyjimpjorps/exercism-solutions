2b895c5a57e277fbb184b6103c57097300eafb3e
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:34 +0000

    [Sync Iteration] fsharp rational-numbers

---
+++ solutions/fsharp/rational-numbers/RationalNumbers.fs
@@ -0,0 +1,38 @@
+﻿module RationalNumbers
+
+type Rational = { num: int; den: int }
+
+let rec gcd a b =
+    match b with
+    | 0 -> a
+    | _ -> gcd b (a % b)
+
+let reduce r =
+    let g = gcd r.num r.den
+    { num = r.num / g; den = r.den / g }
+
+let create num den = reduce { num = num; den = den }
+
+let add r1 r2 =
+    reduce
+        { num = r1.num * r2.den + r2.num * r1.den
+          den = r1.den * r2.den }
+
+let sub r1 r2 = add r1 { r2 with num = -1 * r2.num }
+
+let mul r1 r2 =
+    reduce
+        { num = r1.num * r2.num
+          den = r1.den * r2.den }
+
+let div r1 r2 = mul r1 { num = r2.den; den = r2.num }
+
+let abs r =
+    reduce { num = abs r.num; den = abs r.den }
+
+let exprational n r =
+    reduce
+        { num = pown r.num n
+          den = pown r.den n }
+
+let expreal r (n: int) : float = float n ** (float r.num / float r.den)
