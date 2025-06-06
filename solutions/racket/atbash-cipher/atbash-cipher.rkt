9fe3e18658a128bab4d247ac0accd59c94bcc5f1
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:51:18 +0000

    [Sync Iteration] racket atbash-cipher

--- solutions/racket/atbash-cipher/atbash-cipher.rkt
+++ solutions/racket/atbash-cipher/atbash-cipher.rkt
@@ -1,18 +1,41 @@
 #lang racket
 (provide encode
          decode)
+(require threading)
 
-(define substitution
-  (for/hash ([from (in-string (build-string 26 (λ (i) (integer->char (+ i 97)))))]
-             [to (in-string (build-string 26 (λ (i) (integer->char (- 122 i)))))])
-    (values from to)))
+(define (char->index c)
+  (cond [(char-lower-case? c) (- (char->integer c) 97)]
+        [(char-upper-case? c) (- (char->integer c) 65)]))
 
-(define (encode msg)
-  (define sanitized-msg (string-replace (string-downcase msg) #rx"[^a-z0-9]" ""))
-  (string-join (regexp-match* #px".{1,5}"
-                              (list->string (for/list ([from (in-string sanitized-msg)])
-                                              (hash-ref substitution from from))))))
+(define (index->char i) (integer->char (+ i 97)))
 
-(define (decode msg)
-  (list->string (for/list ([from (in-string (string-replace msg " " ""))])
-                  (hash-ref substitution from from))))
+(define/contract (encode msg)
+  (-> string? string?)
+  (define msg-clean
+    (~> msg
+      (string-replace #px"[^[:alnum:]]" "")
+      string-downcase))
+  (define encoded-msg-raw
+    (list->string
+     (for/list ([c (in-string msg-clean)])
+       (if (char-numeric? c)
+           c
+           (~> c
+             char->index
+             (- 25 _)
+             index->char)))))
+  (~> encoded-msg-raw
+    (regexp-match* #px".{5}|.{1,}" _)
+    string-join))
+
+(define/contract (decode msg)
+  (-> string? string?)
+  (list->string
+   (for/list ([c (in-string msg)]
+              #:unless (char-whitespace? c))
+     (if (char-numeric? c)
+         c
+         (~> c
+           char->index
+           (- 25 _)
+           index->char)))))
