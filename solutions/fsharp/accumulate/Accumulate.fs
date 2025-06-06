bb87a943a81ca5c84f952ab9c488f3b5bb26d3a1
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:28 +0000

    [Sync Iteration] fsharp accumulate

---
+++ solutions/fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,9 @@
+﻿module Accumulate
+
+let accumulate (func: 'a -> 'b) (input: 'a list) : 'b list =
+    let rec acc_internal func input acc =
+        match input with
+        | [] -> List.rev acc
+        | car :: cdr -> acc_internal func cdr ((func car) :: acc)
+
+    acc_internal func input []
