c5dc997b1dd0643f9255da6d64bbc793fb4d62df
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:58:20 +0000

    [Sync Iteration] r word-count

--- solutions/r/word-count/word-count.R
+++ solutions/r/word-count/word-count.R
@@ -4,14 +4,14 @@ library(dplyr)
 library(tibble)
 
 word_count <- function(input) {
-  input %>%
-    str_to_lower() %>%
-    str_split_1("[^a-z0-9']+") %>%
-    str_replace_all("^'?(.*?)'?$", "\\1") %>%
-    discard(\(w) w == "") %>%
-    enframe(name = NULL, value = "word") %>%
-    group_by(word) %>%
-    count() %>%
-    deframe() %>%
+  input |>
+    str_to_lower() |>
+    str_split_1("[^a-z0-9']+") |>
+    str_replace_all("^'?(.*?)'?$", "\\1") |>
+    discard(\(w) w == "") |>
+    enframe(name = NULL, value = "word") |>
+    group_by(word) |>
+    count() |>
+    deframe() |>
     as.list()
 }
