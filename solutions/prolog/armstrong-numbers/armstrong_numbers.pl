48c86f7beb1c8b6db467bb4bbbf6ed15275bd71e
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:28 +0000

    [Sync Iteration] prolog armstrong-numbers

--- solutions/prolog/armstrong-numbers/armstrong_numbers.pl
+++ solutions/prolog/armstrong-numbers/armstrong_numbers.pl
@@ -1,10 +1,8 @@
 armstrong_number(N) :-
-    number_chars(N, S),
-    length(S, Len),
-    maplist(char_to_power(Len), S, Digits),
-    foldl(plus, Digits, 0, N1),
+    number_chars(N, S), length(S, Len),
+    foldl(char_to_power(Len), S, 0, N1),
     N1 =:= N.
 
-char_to_power(Len, Char, Result) :-
+char_to_power(Len, Char, Acc, Result) :-
     atom_number(Char, N),
-    Result is N ** Len.
+    Result is Acc + N ** Len.
