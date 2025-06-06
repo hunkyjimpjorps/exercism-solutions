fb8833f2146bca55cbeb5f2dcb9563f68832ae5e
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:26 +0000

    [Sync Iteration] prolog queen-attack

--- solutions/prolog/queen-attack/queen_attack.pl
+++ solutions/prolog/queen-attack/queen_attack.pl
@@ -1,13 +1,13 @@
-create((Rank, File)) :-
-    between(0, 7, Rank),
-    between(0, 7, File).
+%! create(+DimTuple)
+%
+% The create/1 predicate succeeds if the DimTuple contains valid chessboard 
+% dimensions, e.g. (0,0) or (2,4).
+create((DimX, DimY)) :-
+	fail.
 
-attack(Piece1, Piece2) :- 
-    create(Piece1), 
-    create(Piece2), 
-    do_attack(Piece1, Piece2).
-
-do_attack((X, _), (X, _)) :- !.
-do_attack((_, Y), (_, Y)) :- !.
-do_attack((X1, Y1), (X2, Y2)) :-
-    abs(X1 - X2) =:= abs(Y1 - Y2).
+%! attack(+FromTuple, +ToTuple)
+%
+% The attack/2 predicate succeeds if a queen positioned on ToTuple is 
+% vulnerable to an attack by another queen positioned on FromTuple.
+attack((FromX, FromY), (ToX, ToY)):-
+	fail.
