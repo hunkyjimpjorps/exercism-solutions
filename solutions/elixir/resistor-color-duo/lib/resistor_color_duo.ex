8793c34a66ece9f1868384f53091c015e8b89235
root <root@ip-10-1-1-60.eu-west-2.compute.internal>
2025-06-06 05:20:59 +0000

    [Sync Iteration] elixir resistor-color-duo

---
+++ solutions/elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -0,0 +1,11 @@
+defmodule ResistorColorDuo do
+  @colors %{black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, grey: 8, white: 9}
+  @doc """
+  Calculate a resistance value from two colors
+  """
+  @spec value(colors :: [atom]) :: integer
+  def value([tens, ones]) do
+    @colors[tens] * 10 + @colors[ones]
+  end
+  def value([tens, ones | _rest]), do: value([tens, ones])
+end
