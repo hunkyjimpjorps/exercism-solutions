425d920dc8fae3fa764a414b43323179d088150f
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:30 +0000

    [Sync Iteration] fsharp robot-name

---
+++ solutions/fsharp/robot-name/RobotName.fs
@@ -0,0 +1,37 @@
+module RobotName
+
+type Robot = { name: string }
+type RobotReply = NextName of AsyncReplyChannel<string>
+
+let r = System.Random()
+let shuffle (r: System.Random) = List.sortBy (fun _ -> r.Next())
+
+let startingNames =
+    [ for c1 in 'A' .. 'Z' do
+          for c2 in 'A' .. 'Z' do
+              for num in 1..999 -> $"{c1}{c2}{num:D3}" ]
+    |> shuffle r
+
+let robotNameGenerator =
+    MailboxProcessor.Start(fun inbox ->
+        let rec loop names =
+            async {
+                let! msg = inbox.Receive()
+
+                match msg with
+                | NextName(channel) ->
+                    channel.Reply(List.head names)
+                    return! loop (List.tail names)
+            }
+
+        loop startingNames)
+
+let mkRobot () : Robot =
+    let new_name = robotNameGenerator.PostAndReply(fun c -> NextName c)
+    { name = new_name }
+
+let name (robot: Robot) : string = robot.name
+
+let reset (robot: Robot) : Robot =
+    let new_name = robotNameGenerator.PostAndReply(fun c -> NextName c)
+    { robot with name = new_name }
