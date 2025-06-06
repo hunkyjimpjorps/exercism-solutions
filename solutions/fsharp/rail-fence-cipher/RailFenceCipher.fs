c55eab07bb83f787e804cdd91ded8b29ac4ce734
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:20 +0000

    [Sync Iteration] fsharp rail-fence-cipher

--- solutions/fsharp/rail-fence-cipher/RailFenceCipher.fs
+++ solutions/fsharp/rail-fence-cipher/RailFenceCipher.fs
@@ -1,7 +1,6 @@
 ﻿module RailFenceCipher
 
 let template rails =
-    // the row pattern goes 0, 1, ... n-1, n, n-1, ... 1, 0, 1 ...
     seq {
         while true do
             yield! [| 0 .. rails - 1 |]
@@ -9,24 +8,15 @@ let template rails =
     }
 
 let encode (rails: int) (message: string) =
-    // attach row numbers in the template to characters
     Seq.zip (template rails) message
-    // sort by the row numbers
     |> Seq.sortBy fst
-    // drop the row numbers and assemble the string
     |> Seq.map snd
     |> System.String.Concat
 
 let decode rails (message: string) =
-    // attach row numbers to each place in the template
     Seq.zip (template rails) [| 1 .. message.Length |]
-    // sort by the row numbers, so that all the row 0 positions are first
-    // then the row 1 positions, etc.
     |> Seq.sortBy fst
-    // attach the characters to the template positions
     |> Seq.zip message
-    // sort by the template positions
     |> Seq.sortBy (snd >> snd)
-    // retrieve the characters and assemble the string
     |> Seq.map fst 
     |> System.String.Concat
