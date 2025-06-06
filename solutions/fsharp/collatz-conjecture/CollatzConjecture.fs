998d47d98b3883cffc9ecc4f30be050808c94c82
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:28 +0000

    [Sync Iteration] fsharp collatz-conjecture

---
+++ solutions/fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,12 @@
+﻿module CollatzConjecture
+
+let steps (number: int) : int option =
+    let rec nextStep (number: int) (step: int) : int option =
+        match number with
+        | 1 -> Some step
+        | n when n % 2 = 0 -> nextStep (n / 2) (step + 1)
+        | n -> nextStep (3 * n + 1) (step + 1)
+
+    match number with
+    | n when n <= 0 -> None
+    | n -> nextStep n 0
