3f0e0044819ee8021e44f5b47176785b7b077746
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:52:00 +0000

    [Sync Iteration] julia all-your-base

---
+++ solutions/julia/all-your-base/all-your-base.jl
@@ -0,0 +1,32 @@
+using Pipe:@pipe
+
+function all_your_base(digits, base_in, base_out)
+    if base_in <= 1
+        throw(DomainError(base_in))
+    elseif base_out <= 1
+        throw(DomainError(base_out))
+    elseif !isnothing(findfirst(x -> x < 0 || x >= base_in, digits))
+        throw(DomainError(digits))
+    elseif isempty(digits)
+        return([0])
+    end
+
+    base10 = @pipe digits |>
+        reverse |>
+        zip(_, Iterators.countfrom(0)) |>
+        sum(((n, i),) -> n * base_in^i, _) |> 
+        to_base_b(_, base_out, Int[]) |>
+        if isempty(_)
+        [0]
+    else
+        identity(_)
+    end
+end
+
+function to_base_b(n, b, acc)
+    if n == 0
+        acc
+    else
+        to_base_b(n ÷ b, b, insert!(acc, 1, n % b))
+    end
+end 
