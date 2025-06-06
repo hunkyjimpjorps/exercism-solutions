9abeea99a01836b26ff8d9171b7ca7304dad7bfd
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:28 +0000

    [Sync Iteration] fsharp allergies

---
+++ solutions/fsharp/allergies/Allergies.fs
@@ -0,0 +1,22 @@
+﻿module Allergies
+
+open System
+
+[<Flags>]
+type Allergen =
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
+
+let list (codedAllergies: int) =
+    Enum.GetValues(typeof<Allergen>) :?> (Allergen [])
+    |> Array.toList
+    |> List.filter (fun a -> a &&& enum<Allergen> (codedAllergies) = a)
+
+let allergicTo (codedAllergies: int) (allergen: Allergen) : bool =
+    Seq.contains allergen (list codedAllergies)
