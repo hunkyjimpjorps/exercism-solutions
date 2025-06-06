269e712f0ff537691a3964a89ef23a4cf83c699e
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:56:39 +0000

    [Sync Iteration] elixir bird-count

--- solutions/elixir/bird-count/lib/bird_count.ex
+++ solutions/elixir/bird-count/lib/bird_count.ex
@@ -5,14 +5,9 @@ defmodule BirdCount do
   def increment_day_count([]), do: [1]
   def increment_day_count([today | rest]), do: [today + 1 | rest]
 
-  def has_day_without_birds?([]), do: false
-  def has_day_without_birds?([0 | _]), do: true
-  def has_day_without_birds?([_ | rest]), do: has_day_without_birds?(rest)
+  def has_day_without_birds?(list), do: 0 in list
 
-  def total([]), do: 0
-  def total([today | rest]), do: today + total(rest)
+  def total(list), do: Enum.sum(list)
 
-  def busy_days([]), do: 0
-  def busy_days([today | rest]) when today >= 5, do: 1 + busy_days(rest)
-  def busy_days([_ | rest]), do: busy_days(rest)
+  def busy_days(list), do: Enum.filter(list, &(&1 >= 5)) |> length()
 end
