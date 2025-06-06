072706428f2f87d005266d4e407688da1b870d4e
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:56:12 +0000

    [Sync Iteration] elixir roman-numerals

--- solutions/elixir/roman-numerals/lib/roman_numerals.ex
+++ solutions/elixir/roman-numerals/lib/roman_numerals.ex
@@ -1,39 +1,33 @@
 defmodule RomanNumerals do
+  @to_roman [
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"}
+  ]
+
   @doc """
   Convert the number to a roman number.
   """
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
-    to_roman = [
-      {1000, "M"},
-      {900, "CM"},
-      {500, "D"},
-      {400, "CD"},
-      {100, "C"},
-      {90, "XC"},
-      {50, "L"},
-      {40, "XL"},
-      {10, "X"},
-      {9, "IX"},
-      {5, "V"},
-      {4, "IV"},
-      {1, "I"}
-    ]
-
-    check_top_digit(number, to_roman)
+    check_top_digit(number, @to_roman)
   end
 
   @spec check_top_digit(pos_integer, [{pos_integer, String.t()}]) :: String.t()
-  def check_top_digit(_, []) do
-    ""
-  end
+  defp check_top_digit(_, []), do: ""
 
-  def check_top_digit(number, conversion) do
-    [{diff, sym} | tail] = conversion
+  defp check_top_digit(number, [{diff, sym} | tail] = conversion) when number >= diff,
+    do: sym <> check_top_digit(number - diff, conversion)
 
-    cond do
-      number >= diff -> sym <> check_top_digit(number - diff, conversion)
-      true -> check_top_digit(number, tail)
-    end
-  end
+  defp check_top_digit(number, [_ | tail]), do: check_top_digit(number, tail)
 end
