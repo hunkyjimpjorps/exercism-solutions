617a8c599ba101585bed76292b771082cb48b2cd
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:28 +0000

    [Sync Iteration] fsharp queen-attack

---
+++ solutions/fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,32 @@
+﻿module QueenAttack
+
+let onBoard = [| 0 .. 7 |]
+
+let (|OnBoard|_|) (r, c) =
+    if Array.contains r onBoard
+       && Array.contains c onBoard then
+        Some()
+    else
+        None
+
+let create (position: int * int) =
+    match position with
+    | OnBoard -> true
+    | _ -> false
+
+let (|InSameRank|_|) ((r1, _), (r2, _)) = if r1 = r2 then Some() else None
+
+let (|InSameFile|_|) ((_, c1), (_, c2)) = if c1 = c2 then Some() else None
+
+let (|Diagonal|_|) ((r1, c1), (r2, c2)) =
+    if abs (r1 - r2) = abs (c1 - c2) then
+        Some()
+    else
+        None
+
+let canAttack (queen1: int * int) (queen2: int * int) =
+    match queen1, queen2 with
+    | InSameRank
+    | InSameFile
+    | Diagonal -> true
+    | _ -> false
