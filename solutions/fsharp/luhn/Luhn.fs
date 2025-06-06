10fdcda0e029e6f83083b9116b75d8c8e6d799a1
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:33 +0000

    [Sync Iteration] fsharp luhn

--- solutions/fsharp/luhn/Luhn.fs
+++ solutions/fsharp/luhn/Luhn.fs
@@ -2,19 +2,29 @@
 
 open System.Text.RegularExpressions
 
-let valid (number: string) =
-    let numberTrimmed = Regex.Replace(number, " ", "")
+let (|InvalidCharacter|) (n: string) = Regex.IsMatch(n, "[^0-9]")
+let (|TooShort|) (n: string) = n.Length <= 1
 
-    not (Regex.IsMatch(numberTrimmed, "[^0-9]"))
-    && numberTrimmed.Length > 1
-    && 0 = (numberTrimmed
-            |> fun n -> Regex.Replace(n, " ", "")
-            |> Seq.rev
-            |> Seq.map (string >> int)
-            |> Seq.indexed
-            |> Seq.sumBy
-                (function
-                | i, n when i % 2 = 0 -> n
-                | _, n when 2 * n > 9 -> 2 * n - 9
-                | _, n -> 2 * n)
-            |> (fun n -> n % 10))
+let (|InvalidChecksum|) (n: string) =
+    n
+    |> fun n -> Regex.Replace(n, " ", "")
+    |> Seq.rev
+    |> Seq.map (string >> int)
+    |> Seq.indexed
+    |> Seq.sumBy
+        (function
+        | i, n when i % 2 = 0 -> n
+        | _, n when n >= 5 -> 2 * n - 9
+        | _, n -> 2 * n)
+    |> (fun n -> n % 10)
+    |> (<>) 0
+
+let valid (number: string) : bool =
+
+    number
+    |> fun n -> Regex.Replace(n, " ", "")
+    |> function
+        | InvalidCharacter true
+        | TooShort true
+        | InvalidChecksum true -> false
+        | _ -> true
