4bbd55cbbe608b7daa61a59f1084956e83c92e99
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:31 +0000

    [Sync Iteration] fsharp series

---
+++ solutions/fsharp/series/Series.fs
@@ -0,0 +1,14 @@
+﻿module Series
+open System
+
+let (|InvalidWindow|) ((str: string), length) : bool = length <= 0 || length > str.Length
+
+let slices (str: string) length =
+    match (str, length) with
+    | InvalidWindow true -> None
+    | _ ->
+        str
+        |> Seq.windowed length
+        |> Seq.map (fun xs -> String.Concat(xs))
+        |> Seq.toList
+        |> Some
