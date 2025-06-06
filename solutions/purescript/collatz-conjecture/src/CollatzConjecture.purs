49c07233d338eeec542bea69010483db1c45c896
root <root@ip-10-1-0-222.eu-west-2.compute.internal>
2025-06-06 09:58:16 +0000

    [Sync Iteration] purescript collatz-conjecture

--- solutions/purescript/collatz-conjecture/src/CollatzConjecture.purs
+++ solutions/purescript/collatz-conjecture/src/CollatzConjecture.purs
@@ -3,15 +3,10 @@ module CollatzConjecture
   ) where
 
 import Prelude
-
-import Data.Int (even)
 import Data.Maybe (Maybe(..))
 
 collatz :: Int -> Maybe Int
 collatz n | n <= 0 = Nothing
-collatz n = Just $ doCollatz 0 n
-  where
-  doCollatz acc x
-    | x == 1 = acc
-    | even x = doCollatz (acc + 1) (x / 2)
-    | otherwise = doCollatz (acc + 1) (3 * x + 1)
+collatz 1 = Just 0
+collatz n | n `mod` 2 == 0 = ((+) 1) <$> collatz (n / 2)
+collatz n = ((+) 1) <$> collatz (3 * n + 1)
