2f2cadf685cde6a1639c934c5308589e65f9a6f4
root <root@ip-10-1-1-203.eu-west-2.compute.internal>
2025-06-06 05:50:55 +0000

    [Sync Iteration] elixir guessing-game

--- solutions/elixir/guessing-game/lib/guessing_game.ex
+++ solutions/elixir/guessing-game/lib/guessing_game.ex
@@ -1,7 +1,8 @@
 defmodule GuessingGame do
+  def compare(secret_number, :no_guess), do: "Make a guess"
   def compare(secret_number, secret_number), do: "Correct"
-  def compare(secret_number, guess \\ :no_guess) when guess == :no_guess, do: "Make a guess"
   def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
   def compare(secret_number, guess) when secret_number > guess, do: "Too low"
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
+  def compare(secret_number), do: "Make a guess"
 end
