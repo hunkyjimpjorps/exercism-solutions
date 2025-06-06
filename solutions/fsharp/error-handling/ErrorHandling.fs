acdd8b90baf4c25cfce46beacc580282a4718b6b
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:30 +0000

    [Sync Iteration] fsharp error-handling

---
+++ solutions/fsharp/error-handling/ErrorHandling.fs
@@ -0,0 +1,25 @@
+﻿module ErrorHandling
+open System
+
+let handleErrorByThrowingException () = failwith "This is an exception"
+
+let handleErrorByReturningOption input =
+    match input with
+    | "1" -> Some 1
+    | _ -> None
+
+let handleErrorByReturningResult input =
+    match input with
+    | "1" -> Ok 1
+    | _ -> Error "Could not convert input to integer"
+
+let bind switchFunction twoTrackInput =
+    match twoTrackInput with
+    | Ok s -> switchFunction s
+    | Error f -> Error f
+
+let cleanupDisposablesWhenThrowingException (resource : IDisposable) =
+    try
+        failwith "This is an exception"
+    finally
+        resource.Dispose()
