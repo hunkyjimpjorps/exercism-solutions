8dba7676081b7f8548819c4c63505d8de9812a10
root <root@ip-10-1-1-81.eu-west-2.compute.internal>
2025-06-06 02:43:30 +0000

    [Sync Iteration] fsharp isogram

---
+++ solutions/fsharp/isogram/Isogram.fs
@@ -0,0 +1,9 @@
+﻿module Isogram
+
+open System.Text.RegularExpressions
+
+let isIsogram (str: string) =
+    let cleanedStr =
+        Regex.Replace(str, "[-\s]", "").ToLower()
+
+    cleanedStr |> Seq.distinct |> Seq.length = cleanedStr.Length
